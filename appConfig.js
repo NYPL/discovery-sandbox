17db1a9925f485aaddbe196ba16ae5df250ebf7a
Edwin Guzman <edwin.gzmn@gmail.com>
2016-08-15 18:21:10 -0400

    Adding search component but not functional yet.

--- appConfig.js
+++ appConfig.js
@@ -1,6 +1,6 @@
 export default {
-  appTitle: 'NYPL | App Name',
-  appName: 'NYPL App',
+  appTitle: 'NYPL | Discovery',
+  appName: 'discovery',
   favIconPath: 'http://ux-static.nypl.org.s3-website-us-east-1.amazonaws.com/images/favicon.ico',
   port: 3001,
   webpackDevServerPort: 3000,
