fb6d55cb0d9f96b49966aa88bdce36bee642df79
Edwin Guzman <edwin.gzmn@gmail.com>
2016-10-19 15:40:59 -0400

    Adding server and client routes for advanced search, holds page, and account pages. Also including dummy content components.

---
+++ src/app/components/Account/Account.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Account extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Account Page</h2>
+      </div>
+    );
+  }
+}
+
+export default Account;
---
+++ src/app/components/Account/AccountHolds.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AccountHolds extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Account Holds</h2>
+      </div>
+    );
+  }
+}
+
+export default AccountHolds;
