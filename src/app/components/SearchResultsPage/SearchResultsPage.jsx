6be85dd00db824e3fd30fb448bda4d3e5d85a811
Edwin Guzman <edwin.gzmn@gmail.com>
2016-11-22 16:29:12 -0500

    Cleaning up and removing code from search results component.

--- src/app/components/SearchResultsPage/SearchResultsPage.jsx
+++ src/app/components/SearchResultsPage/SearchResultsPage.jsx
@@ -15,57 +15,7 @@ findIndex as _findIndex } from 'underscore';
 
 class SearchResultsPage extends React.Component {
   componentWillMount() {
-    // if (this.props.location.search.substring(3)) {
-    //   let q = decodeURIComponent(this.props.location.search.substring(3));
-    //   let spaceIndex;
-
-    //   if (q.indexOf(':') !== -1) {
-    //     spaceIndex = (q.substring(0, q.indexOf(':'))).lastIndexOf(' ');
-    //   } else {
-    //     spaceIndex = q.length;
-    //   }
-
-    //   const paramkeywords = q.substring(0, spaceIndex);
-    //   let urlFacet = q.substring(spaceIndex + 1);
-
-    //   let selectedFacets = {};
-
-    //   if (urlFacet) {
-    //     let facetStrArray = q.substring(spaceIndex + 1).replace(/\" /, '"#').split('#');
-
-    //     facetStrArray.forEach(str => {
-    //       if (!str) return;
-
-    //       // Each string appears like so: 'contributor:"United States. War Department."'
-    //       // Can't simply split by ':' because some strings are: 'owner:"orgs:1000"'
-    //       const field = str.split(':"')[0];
-    //       const value = str.split(':"')[1].replace('"', '');
-
-    //       // console.log(field, this.props.facets.itemListElement)
-    //       let index = _findIndex(this.props.facets.itemListElement, { field });
-    //       let v = _findWhere(this.props.facets.itemListElement[index].values, { value });
-    //       console.log(v);
-    //       selectedFacets[field] = {
-    //         id: v.value,
-    //         value: v.label || v.value,
-    //       };
-    //     });
-    //   }
-
-    //   Actions.updateSelectedFacets(selectedFacets);
-    //   axios
-    //     .get(`/api?q=${q}`)
-    //     .then(response => {
-    //       Actions.updateSearchResults(response.data.searchResults);
-    //       Actions.updateFacets(response.data.facets);
-    //       Actions.updateSearchKeywords(paramkeywords);
-    //     })
-    //     .catch(error => {
-    //       console.log(error);
-    //     });
-    // } else
     if (!this.props.searchResults) {
-      console.log('test');
       axios
         .get(`/api?q=${this.props.searchKeywords}`)
         .then(response => {
