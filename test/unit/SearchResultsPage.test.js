c3f5a61de5a9db9172a7957a6c7636810031ddcd
Edwin Guzman <edwin.gzmn@gmail.com>
2017-01-05 12:08:54 -0500

    Small cleanup and updating tests.

--- test/unit/SearchResultsPage.test.js
+++ test/unit/SearchResultsPage.test.js
@@ -10,6 +10,7 @@ const mock = new MockAdapter(axios);
 
 import SearchResultsPage from '../../src/app/components/SearchResultsPage/SearchResultsPage.jsx';
 import Actions from '../../src/app/actions/Actions.js';
+import FacetSidebar from '../../src/app/components/FacetSidebar/FacetSidebar.jsx';
 
 // Eventually, it would be nice to have mocked data in a different file and imported.
 const searchResults = {
@@ -29,104 +30,130 @@ const searchResults = {
 };
 
 describe('SearchResultsPage', () => {
-  let component;
+  describe('Component properties', () => {
+    let component;
 
-  before(() => {
-    // Added this empty prop so that the `componentWillMount` method will be skipped.
-    // That lifecycle hook is tested later on.
-    component = mount(<SearchResultsPage searchResults={{}} />);
+    before(() => {
+      // Added this empty prop so that the `componentWillMount` method will be skipped.
+      // That lifecycle hook is tested later on.
+      component = mount(<SearchResultsPage searchResults={{}} />);
+    });
+
+    it('should be wrapped in a .mainContent', () => {
+      expect(component.find('#mainContent')).to.have.length(1);
+    });
+
+    it('should render a <Search /> components', () => {
+      expect(component.find('Search')).to.have.length(1);
+    });
+
+    it('should render a <Breadcrumbs /> components', () => {
+      expect(component.find('Breadcrumbs')).to.have.length(1);
+    });
+
+    it('should render a <FacetSidebar /> components', () => {
+      expect(component.find('FacetSidebar')).to.have.length(1);
+    });
+
+    it('should render a <Hits /> components', () => {
+      expect(component.find('Hits')).to.have.length(1);
+    });
+
+    it('should render a <Results /> components', () => {
+      expect(component.find('Results')).to.have.length(1);
+    });
+
+    it('should have empty search results', () => {
+      // eql is deep equal
+      // In order to test for props, the components needs to be mounted with `enzyme.mount`.
+      expect(component.props().searchResults).to.eql({});
+    });
   });
 
-  it('should be wrapped in a .mainContent', () => {
-    expect(component.find('#mainContent')).to.have.length(1);
+  describe('With passed search results prop', () => {
+    let component;
+    let spyUpdateSearchResults;
+    let spyUpdateSearchKeywords;
+
+    before(() => {
+      component = mount(
+        <SearchResultsPage searchKeywords="locofocos" searchResults={searchResults} />
+      );
+      spyUpdateSearchResults = sinon.spy(Actions, 'updateSearchResults');
+      spyUpdateSearchKeywords = sinon.spy(Actions, 'updateSearchKeywords');
+    });
+
+    after(() => {
+      Actions.updateSearchResults.restore();
+      Actions.updateSearchKeywords.restore();
+    })
+
+    it('should have search results', () => {
+      expect(component.props().searchResults).to.be.defined;
+    });
+
+    it('should have two results in the `itemListElement` array', () => {
+      expect(component.props().searchResults.itemListElement).to.have.length(2);
+    });
+
+    it('should not call two Action functions since data is being passed', () => {
+      expect(spyUpdateSearchResults.callCount).to.equal(0);
+      expect(spyUpdateSearchKeywords.callCount).to.equal(0);
+    });
   });
 
-  it('should render a <Search /> components', () => {
-    expect(component.find('Search')).to.have.length(1);
+  // Mounting the SearchResultsPage without searchResults prop will make it fetch data. This is for
+  // a use case when navigation back in the history through the brower's back button. Still needs
+  // to be fully tested and updated.
+  describe('Fetching data', () => {
+    let component;
+    let spyUpdateSearchResults;
+    let spyUpdateSearchKeywords;
+
+    before(() => {
+      spyUpdateSearchResults = sinon.spy(Actions, 'updateSearchResults');
+      spyUpdateSearchKeywords = sinon.spy(Actions, 'updateSearchKeywords');
+
+      mock
+        .onGet('/api?q=locofocos')
+        .reply(200, { searchResults });
+
+      component = mount(<SearchResultsPage searchKeywords="locofocos" />);
+    });
+
+    after(() => {
+      mock.reset();
+      Actions.updateSearchResults.restore();
+      Actions.updateSearchKeywords.restore();
+    });
+
+    it('should call two Action functions after the ajax call for data', () => {
+      expect(spyUpdateSearchResults.callCount).to.equal(1);
+      expect(spyUpdateSearchKeywords.callCount).to.equal(1);
+    });
   });
 
-  it('should render a <Breadcrumbs /> components', () => {
-    expect(component.find('Breadcrumbs')).to.have.length(1);
-  });
+  describe('With facet data as prop', () => {
+    const facets = { itemListElement: [] };
+    let component;
 
-  it('should render a <FacetSidebar /> components', () => {
-    expect(component.find('FacetSidebar')).to.have.length(1);
-  });
+    before(() => {
+      component = mount(
+        <SearchResultsPage searchResults={{}} searchKeywords="locofocos" facets={facets} />
+      );
+    });
 
-  it('should render a <Hits /> components', () => {
-    expect(component.find('Hits')).to.have.length(1);
-  });
+    after(() => {
+    });
 
-  it('should render a <Results /> components', () => {
-    expect(component.find('Results')).to.have.length(1);
-  });
-
-  it('should have empty search results', () => {
-    // eql is deep equal
-    // In order to test for props, the components needs to be mounted with `enzyme.mount`.
-    expect(component.props().searchResults).to.eql({});
-  });
-});
-
-describe('SearchResultsPage with passed search results prop', () => {
-  let component;
-  let spyUpdateSearchResults;
-  let spyUpdateSearchKeywords;
-
-  before(() => {
-    component = mount(
-      <SearchResultsPage searchKeywords="locofocos" searchResults={searchResults} />
-    );
-    spyUpdateSearchResults = sinon.spy(Actions, 'updateSearchResults');
-    spyUpdateSearchKeywords = sinon.spy(Actions, 'updateSearchKeywords');
-  });
-
-  after(() => {
-    spyUpdateSearchResults.restore();
-    spyUpdateSearchKeywords.restore();
-  })
-
-  it('should have search results', () => {
-    expect(component.props().searchResults).to.be.defined;
-  });
-
-  it('should have two results in the `itemListElement` array', () => {
-    expect(component.props().searchResults.itemListElement).to.have.length(2);
-  });
-
-  it('should not call two Action functions since data is being passed', () => {
-    expect(spyUpdateSearchResults.callCount).to.equal(0);
-    expect(spyUpdateSearchKeywords.callCount).to.equal(0);
-  });
-});
-
-// Mounting the SearchResultsPage without searchResults prop will make it fetch data. This is for
-// a use case when navigation back in the history through the brower's back button. Still needs
-// to be fully tested and updated.
-describe('SearchResultsPage fetching data', () => {
-  let component;
-  let spyUpdateSearchResults;
-  let spyUpdateSearchKeywords;
-
-  before(() => {
-    spyUpdateSearchResults = sinon.spy(Actions, 'updateSearchResults');
-    spyUpdateSearchKeywords = sinon.spy(Actions, 'updateSearchKeywords');
-
-    mock
-      .onGet('/api?q=locofocos')
-      .reply(200, { searchResults });
-
-    component = mount(<SearchResultsPage searchKeywords="locofocos" />);
-  });
-
-  after(() => {
-    mock.restore();
-    spyUpdateSearchResults.restore();
-    spyUpdateSearchKeywords.restore();
-  });
-
-  it('should call two Action functions after the ajax call for data', () => {
-    expect(spyUpdateSearchResults.callCount).to.equal(1);
-    expect(spyUpdateSearchKeywords.callCount).to.equal(1);
+    it('should contain FacetSidebar', () => {
+      expect(component.contains(<FacetSidebar
+        facets={[]}
+        selectedFacets={undefined}
+        keywords={'locofocos'}
+        sortBy={undefined}
+        className="quarter"
+      />)).to.be.true;
+    });
   });
 });
