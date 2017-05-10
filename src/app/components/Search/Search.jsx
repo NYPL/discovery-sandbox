a485f29dacba4b3e8db435516b391fc37652ec5a
Ashley Blewer <ashley.blewer@gmail.com>
2017-05-10 14:15:34 -0400

    resets all selected facets when searching

--- src/app/components/Search/Search.jsx
+++ src/app/components/Search/Search.jsx
@@ -13,6 +13,7 @@ import {
 
 import {
   extend as _extend,
+  forEach as _forEach,
 } from 'underscore';
 
 /**
@@ -65,6 +66,11 @@ class Search extends React.Component {
     ajaxCall(`/api?q=${keyword}${sortQuery}`, (response) => {
       Actions.updateSearchResults(response.data.searchResults);
       Actions.updateFacets(response.data.facets);
+      const newFacets = {};
+      _forEach(response.data.facets.itemListElement, (facet) => {
+        newFacets[facet.id] = { id: '', value: '' };
+      });
+      Actions.updateSelectedFacets(newFacets);
       Actions.updateSearchKeywords(keyword);
       Actions.updatePage('1');
       this.routeHandler({
