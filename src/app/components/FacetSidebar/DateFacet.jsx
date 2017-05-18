8acc711aa49b1dcdf027c4056093bce973c21ba4
Edwin Guzman <edwin.gzmn@gmail.com>
2017-05-18 17:35:05 -0400

    Adding ability for search utility function to use filters and basic dates functionality.

--- src/app/components/FacetSidebar/DateFacet.jsx
+++ src/app/components/FacetSidebar/DateFacet.jsx
@@ -2,19 +2,15 @@ import React from 'react';
 
 import {
   extend as _extend,
-  findWhere as _findWhere,
-  pick as _pick,
 } from 'underscore';
 
 import Actions from '../../actions/Actions';
 import Store from '../../stores/Store';
 import {
   ajaxCall,
-  getSortQuery,
   getFacetParams,
+  getFieldParam,
 } from '../../utils/utils';
-
-const facetShowLimit = 4;
 
 class DateFacet extends React.Component {
   constructor(props) {
@@ -22,19 +18,18 @@ class DateFacet extends React.Component {
 
     this.state = _extend({
       spinning: false,
-      openFacet: true,
+      dateAfter: '',
+      dateBefore: '',
     }, Store.getState());
 
     this.onChange = this.onChange.bind(this);
     this.routeHandler = this.routeHandler.bind(this);
+    this.inputChange = this.inputChange.bind(this);
+    this.triggerSubmit = this.triggerSubmit.bind(this);
   }
 
   componentDidMount() {
     Store.listen(this.onChange);
-
-    setTimeout(() => {
-      this.setState({ openFacet: false });
-    }, 500);
   }
 
   componentWillUnmount() {
@@ -46,40 +41,69 @@ class DateFacet extends React.Component {
   }
 
   routeHandler(path) {
-    if (path === '/') {
-      Actions.updateSelectedFacets({});
-    }
-
     this.context.router.push(path);
+  }
+
+  inputChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+  triggerSubmit(event) {
+    if (event && event.charCode === 13) {
+      Actions.updateSpinner(true);
+      let dateFilters = '';
+
+      if (this.state.dateAfter) {
+        dateFilters = `&filters[dateAfter]=${this.state.dateAfter}`;
+      }
+      if (this.state.dateBefore) {
+        dateFilters += `&filters[dateBefore]=${this.state.dateBefore}`;
+      }
+
+      const fieldQuery = getFieldParam(this.state.field);
+
+      ajaxCall(`/api?q=${this.props.keywords}${dateFilters}${fieldQuery}`, (response) => {
+        Actions.updateSearchResults(response.data.searchResults);
+        Actions.updateFacets(response.data.facets);
+        Actions.updatePage('1');
+        this.routeHandler(
+          `/search?q=${encodeURIComponent(this.props.keywords)}${dateFilters}${fieldQuery}`
+        );
+        Actions.updateSpinner(false);
+      });
+    }
   }
 
   render() {
     const spinningClass = this.state.spinning ? 'spinning' : '';
 
     return (
-      <div className={`nypl-facet-search nypl-spinner-field ${this.state.spinning ? 'spinning' : ''}`}>
+      <div className={`nypl-facet-search nypl-spinner-field ${spinningClass}`}>
         <div className="nypl-text-field">
-          <label
-            key="date-from"
-            htmlFor="date-from"
-          >On or After Year</label>
+          <label key="date-from" htmlFor="facet-date-from-search">On or After Year</label>
           <input
-            id={`facet-date-from-search`}
+            id="facet-date-from-search"
             type="text"
             className="form-text"
             placeholder=""
+            name="dateAfter"
+            onChange={this.inputChange}
+            value={this.state.dateAfter}
+            onKeyPress={this.triggerSubmit}
           />
         </div>
         <div className="nypl-text-field">
-          <label
-            key="date-to"
-            htmlFor="date-to"
-          >On or Before Year</label>
+          <label key="date-to" htmlFor="facet-date-to-search">On or Before Year</label>
           <input
-            id={`facet-date-to-search`}
+            id="facet-date-to-search"
             type="text"
             className="form-text"
             placeholder=""
+            name="dateBefore"
+            onChange={this.inputChange}
+            value={this.state.dateBefore}
+            onKeyPress={this.triggerSubmit}
           />
         </div>
       </div>
@@ -88,9 +112,8 @@ class DateFacet extends React.Component {
 }
 
 DateFacet.propTypes = {
-  sortBy: React.PropTypes.string,
-  totalHits: React.PropTypes.number,
-  selectedValue: React.PropTypes.string,
+  keywords: React.PropTypes.string,
+  selectedFacets: React.PropTypes.object,
 };
 
 DateFacet.contextTypes = {
