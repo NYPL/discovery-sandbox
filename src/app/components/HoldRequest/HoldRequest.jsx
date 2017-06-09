ee534e19041b635e22ca3b77084d94f217c5be1e
Kang Peng <kangtingpeng@nypl.org>
2017-06-09 17:37:24 -0400

    Syntax updated.

--- src/app/components/HoldRequest/HoldRequest.jsx
+++ src/app/components/HoldRequest/HoldRequest.jsx
@@ -81,67 +81,71 @@ class HoldRequest extends React.Component {
     const day = date.getDate();
     const monthIndex = date.getMonth();
     const dateDisplay = `${monthNames[monthIndex]} ${day}`;
-    const content = (record) ? (
-      <div className="content-wrapper">
-        <div className="item-header">
-          <h1>Research item hold request</h1>
-        </div>
+    let content = null;
 
-        <div className="item-summary">
-          <div className="item">
-            <h2>You are about to request a hold on the following research item:</h2>
-            <Link href={`/item/${bibId}`}>{title}</Link>
+    if (record) {
+      content =
+        <div className="content-wrapper">
+          <div className="item-header">
+            <h1>Research item hold request</h1>
           </div>
-        </div>
 
-        <form className="place-hold-form form" action={`/hold/request/${itemId}`} method="POST">
-          <h2>Confirm account</h2>
-          {this.renderLoggedInInstruction(patronName)}
-          <h2>Confirm delivery location</h2>
-          <p>When this item is ready, you will use it in the following location:</p>
-          <fieldset className="select-location-fieldset">
-            <legend className="visuallyHidden">Select a pickup location</legend>
-            <div className="group selected">
-              <span className="col location">
-                <a href={`${location.uri}`}>{location['full-name']}</a><br />{location.address.address1}<br />
-                {location.prefLabel}
-                {location.offsite &&
-                  <span>
-                    <br /><small>(requested from offsite storage)</small><br />
+          <div className="item-summary">
+            <div className="item">
+              <h2>You are about to request a hold on the following research item:</h2>
+              <Link href={`/item/${bibId}`}>{title}</Link>
+            </div>
+          </div>
+
+          <form className="place-hold-form form" action={`/hold/request/${itemId}`} method="POST">
+            <h2>Confirm account</h2>
+            {this.renderLoggedInInstruction(patronName)}
+            <h2>Confirm delivery location</h2>
+            <p>When this item is ready, you will use it in the following location:</p>
+            <fieldset className="select-location-fieldset">
+              <legend className="visuallyHidden">Select a pickup location</legend>
+              <div className="group selected">
+                <span className="col location">
+                  <a href={`${location.uri}`}>{location['full-name']}</a><br />{location.address.address1}<br />
+                  {location.prefLabel}
+                  {location.offsite &&
+                    <span>
+                      <br /><small>(requested from offsite storage)</small><br />
+                    </span>
+                  }
+                </span>
+                {selectedItem.shelfMark &&
+                  <span className="col">
+                    <small>Call number:</small><br />{selectedItem.shelfMark[0]}
                   </span>
                 }
-              </span>
-              {selectedItem.shelfMark &&
-                <span className="col">
-                  <small>Call number:</small><br />{selectedItem.shelfMark[0]}
-                </span>
-              }
-              {/* <span className="col"><small>Ready by approximately:</small><br />{dateDisplay}, 9am.</span> */}
-            </div>
-          </fieldset>
+                {/* <span className="col"><small>Ready by approximately:</small><br />{dateDisplay}, 9am.</span> */}
+              </div>
+            </fieldset>
 
-          <input type="hidden" name="pickupLocation" value={location.code} />
+            <input type="hidden" name="pickupLocation" value={location.code} />
 
-          <button type="submit" className="large">
-            Submit your item hold request
-          </button>
-        </form>
-      </div>
-    ) : (
-      <div className="content-wrapper">
-        <div className="item-header">
-          <h1>Research item hold request</h1>
-        </div>
-        <div className="item-summary">
-          <div className="item">
-            <h2>Something wrong with your request</h2>
-            <Link href={`/item/${bibId}`}>{title}</Link>
+            <button type="submit" className="large">
+              Submit your item hold request
+            </button>
+          </form>
+        </div>;
+    } else {
+      content =
+        <div className="content-wrapper">
+          <div className="item-header">
+            <h1>Research item hold request</h1>
           </div>
-        </div>
-        <h2>Confirm account</h2>
-        {this.renderLoggedInInstruction(patronName)}
-      </div>
-    );
+          <div className="item-summary">
+            <div className="item">
+              <h2>Something wrong with your request</h2>
+              <Link href={`/item/${bibId}`}>{title}</Link>
+            </div>
+          </div>
+          <h2>Confirm account</h2>
+          {this.renderLoggedInInstruction(patronName)}
+        </div>;
+    }
 
     return (
       <div id="mainContent">
