0071bb9a7ae1384341538eab040861292ced7e1f
Edwin Guzman <edwin.gzmn@gmail.com>
2017-06-22 16:51:46 -0400

    Refactoring out small pieces into their own components.

--- src/app/components/Hits/ClearHits.jsx
+++ src/app/components/Hits/ClearHits.jsx
@@ -3,36 +3,28 @@ import PropTypes from 'prop-types';
 import Actions from '../../actions/Actions';
 import { ajaxCall } from '../../utils/utils';
 
-class ClearHits extends React.Component {
-  constructor(props) {
-    super(props);
-    this.clearResults = this.clearResults.bind(this);
-  }
-
-  clearResults() {
+const ClearHits = (props, context) => {
+  const clearResults = () => {
     Actions.updateSpinner(true);
     Actions.updateSearchKeywords('');
     Actions.updateSelectedFacets({});
-    ajaxCall(`/api`, (response) => {
+    ajaxCall('/api', (response) => {
       Actions.updateSearchResults(response.data.searchResults);
       Actions.updateFacets(response.data.facets);
       Actions.updatePage('1');
-      this.context.router.push(`/search`);
+      context.router.push('/search');
       Actions.updateSpinner(false);
     });
-  }
+  };
 
-  render() {
-    return (
-      <div className="nypl-clear-results">
-        <button
-          className="nypl-link-button"
-          onClick={() => this.clearResults()}
-        >Clear current search query, filters, and sorts</button>
-      </div>
-    )
-  }
-}
+  return (
+    <div className="nypl-clear-results">
+      <button className="nypl-link-button" onClick={() => clearResults()}>
+        Clear current search query, filters, and sorts
+      </button>
+    </div>
+  );
+};
 
 ClearHits.contextTypes = {
   router: PropTypes.object,
