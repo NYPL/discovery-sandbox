96033ffed0c945f41f6e985e6d95fc7fae18151d
Edwin Guzman <edwin.gzmn@gmail.com>
2017-07-03 15:45:26 -0400

    Refactoring code to make the logic that places a hold request easier to manage and reuseable.

--- src/server/ApiRoutes/Hold.js
+++ src/server/ApiRoutes/Hold.js
@@ -4,6 +4,53 @@ import appConfig from '../../../appConfig.js';
 import User from './User.js';
 import Bib from './Bib.js';
 
+function postHoldAPI(req, cb, errorCb) {
+  // retrieve access token and patron info
+  const accessToken = req.tokenResponse.accessToken;
+  const patronId = req.tokenResponse.decodedPatron.sub;
+  const patronHoldsApi = `${appConfig.api.development}/hold-requests`;
+
+  // get item id and pickup location
+  // NOTE: The implementation for this needs to be redone, or
+  // we may get it directly from the API.
+  let itemId = req.params.itemId;
+  let nyplSource = 'sierra-nypl';
+
+  if (itemId.indexOf('-') >= 0) {
+    const parts = itemId.split('-');
+    itemId = parts[parts.length - 1];
+
+    if (itemId.substring(0, 2) === 'pi') {
+      nyplSource = 'recap-PUL';
+    } else if (itemId.substring(0, 2) === 'ci') {
+      nyplSource = 'recap-CUL';
+    }
+  }
+  itemId = itemId.replace(/\D/g, '');
+  const pickupLocation = req.body.pickupLocation;
+
+  const data = {
+    patron: patronId,
+    recordType: 'i',
+    record: itemId,
+    nyplSource,
+    pickupLocation,
+    // neededBy: "2013-03-20",
+    numberOfCopies: 1,
+  };
+  console.log('Making hold request', data, accessToken);
+
+  return axios
+    .post(patronHoldsApi, data, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+    .then(cb)
+    .catch(errorCb);
+}
+
 function confirmRequestServer(req, res, next) {
   const bibId = req.params.bibId || '';
   const loggedIn = User.requireUser(req, res);
@@ -64,59 +111,26 @@ function createHoldRequestServer(req, res) {
   const loggedIn = User.requireUser(req);
   if (!loggedIn) return false;
 
-  // retrieve access token and patron info
-  const accessToken = req.tokenResponse.accessToken;
-  const patronId = req.tokenResponse.decodedPatron.sub;
-  const patronHoldsApi = `${appConfig.api.development}/hold-requests`;
-
-  // get item id and pickup location
-  let itemId = req.params.itemId;
-  let nyplSource = 'sierra-nypl';
-
-  if (itemId.indexOf('-') >= 0) {
-    const parts = itemId.split('-');
-    itemId = parts[parts.length - 1];
-
-    if (itemId.substring(0, 2) === 'pi') {
-      nyplSource = 'recap-PUL';
-    } else if (itemId.substring(0, 2) === 'ci') {
-      nyplSource = 'recap-CUL';
-    }
+  if (!req.params.bibId || !req.params.itemId) {
+    // Dummy redirect for now
+    return res.redirect('/someErrorPage');
   }
-  itemId = itemId.replace(/\D/g, '');
-  const pickupLocation = req.body.pickupLocation;
 
-  const data = {
-    patron: patronId,
-    recordType: 'i',
-    record: itemId,
-    nyplSource,
-    pickupLocation,
-    // neededBy: "2013-03-20",
-    numberOfCopies: 1,
-  };
-  console.log('Making hold request', data, accessToken);
-
-  return axios
-    .post(patronHoldsApi, data, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-    })
-    .then(response => {
+  return postHoldAPI(
+    req,
+    (response) => {
       // console.log('Holds API response:', response);
       console.log('Hold Request Id:', response.data.data.id);
       console.log('Job Id:', response.data.data.jobId);
       res.redirect(`/hold/confirmation/${req.params.bibId}-` +
         `${req.params.itemId}?requestId=${response.data.data.id}`);
-    })
-    .catch(error => {
-      // console.log(error);
+    },
+    (error) => {
       console.log(`Error calling Holds API : ${error.data.message}`);
       res.redirect(`/hold/request/${req.params.bibId}-` +
         `${req.params.itemId}?errorMessage=${error.data.message}`);
-    }); /* end axios call */
+    }
+  );
 }
 
 function createHoldRequestAjax(req, res) {
@@ -124,60 +138,27 @@ function createHoldRequestAjax(req, res) {
   const loggedIn = User.requireUser(req);
   if (!loggedIn) return false;
 
-  // retrieve access token and patron info
-  const accessToken = req.tokenResponse.accessToken;
-  const patronId = req.tokenResponse.decodedPatron.sub;
-  const patronHoldsApi = `${appConfig.api.development}/hold-requests`;
-
-  // get item id and pickup location
-  let itemId = req.query.itemId;
-  let nyplSource = 'sierra-nypl';
-
-  if (itemId.indexOf('-') >= 0) {
-    const parts = itemId.split('-');
-    itemId = parts[parts.length - 1];
-
-    if (itemId.substring(0, 2) === 'pi') {
-      nyplSource = 'recap-PUL';
-    } else if (itemId.substring(0, 2) === 'ci') {
-      nyplSource = 'recap-CUL';
-    }
+  if (!req.params.bibId || !req.params.itemId) {
+    // Dummy redirect for now
+    return res.redirect('/someErrorPage');
   }
-  itemId = itemId.replace(/\D/g, '');
-  const pickupLocation = req.body.pickupLocation;
 
-  const data = {
-    patron: patronId,
-    recordType: 'i',
-    record: itemId,
-    nyplSource,
-    pickupLocation,
-    // neededBy: "2013-03-20",
-    numberOfCopies: 1,
-  };
-  // console.log('Making hold request', data, accessToken);
-
-  return axios
-    .post(patronHoldsApi, data, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-    })
-    .then(response => {
+  return postHoldAPI(
+    req,
+    (response) => {
       res.json({
         id: response.data.data.id,
         jobId: response.data.data.jobId,
-        holdRequest: data,
       });
-    })
-    .catch(error => {
+    },
+    (error) => {
       console.log(`Error calling Holds API : ${error.data.message}`);
       res.json({
         status: error.status,
         error,
       });
-    }); /* end axios call */
+    }
+  );
 }
 
 
