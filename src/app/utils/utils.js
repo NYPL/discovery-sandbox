ec061a11485751a72108718a83b6fb930919c5c2
Kang Peng <kangtingpeng@nypl.org>
2017-07-07 12:18:11 -0400

    Reversed updates for preserving legacy functions.

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -197,36 +197,36 @@ const getFieldParam = (field) => {
 
 function collapse(results) {
   const owiLookup = {};
-  if (!results || !results.searchResults || !results.searchResults.itemListElement) return results;
+  if (!results || !results.searchResults || !results.searchResults.itemListElement) return results
   // make the lookup by owi
   results.searchResults.itemListElement.forEach((r) => {
     if (r.result && r.result.idOwi && r.result.idOwi[0]) {
-      if (!owiLookup[r.result.idOwi[0]]) owiLookup[r.result.idOwi[0]] = [];
-      owiLookup[r.result.idOwi[0]].push(r);
+      if (!owiLookup[r.result.idOwi[0]]) owiLookup[r.result.idOwi[0]] = []
+      owiLookup[r.result.idOwi[0]].push(r)
     }
   });
 
-  const newItemListElement = [];
-  const completedOwis = [];
+  let newItemListElement = [];
+  let completedOwis = [];
 
   results.searchResults.itemListElement.forEach((r) => {
     if (r.result && r.result.idOwi && r.result.idOwi[0] &&
       owiLookup[r.result.idOwi[0]].length > 1) {
       // if we did a result w/ one of the OWIs we did it for all of them
-      if (completedOwis.indexOf(r.result.idOwi[0]) > -1) return;
-      completedOwis.push(r.result.idOwi[0]);
+      if (completedOwis.indexOf(r.result.idOwi[0]) > -1) return
+      completedOwis.push(r.result.idOwi[0])
 
       // there are more than one/none owi matching in this results set,
       // pick the best one and collapse the rest
       // pick one that has a physcial local copy
       let parent = null;
       owiLookup[r.result.idOwi[0]].forEach((i) => {
-        if (parent) return;
+        if (parent) return
         if (i.result && i.result.items) {
           i.result.items.forEach((ii) => {
             if (ii.location && ii.location[0] && ii.location[0][0]) {
               if (ii.location[0][0]['@id'].search(/loc:ma/) > -1) {
-                parent = i;
+                parent = i
               }
             }
           });
@@ -235,26 +235,26 @@ function collapse(results) {
       // pick the first physcial one at recap if no local ver
       if (!parent) {
         owiLookup[r.result.idOwi[0]].forEach((i) => {
-          if (parent) return;
+          if (parent) return
           if (i.result && i.result.items) {
             i.result.items.forEach((ii) => {
               if (ii.location && ii.location[0] && ii.location[0][0]) {
                 if (ii.location[0][0]['@id'].search(/loc:rc/) > -1) {
-                  parent = i;
+                  parent = i
                 }
               }
-            });
+            })
           }
-        });
+        })
       }
       // just select the first one
       if (!parent) {
         owiLookup[r.result.idOwi[0]].forEach((i) => {
-          if (parent) return;
+          if (parent) return
           if (i.result && i.result.items) {
             i.result.items.forEach((ii) => {
               if (ii.location && ii.location[0] && ii.location[0][0]) {
-                parent = i;
+                parent = i
               }
             });
           }
@@ -262,26 +262,26 @@ function collapse(results) {
       }
 
       if (parent) {
-        parent.collapsedBibs = [];
+        parent.collapsedBibs = []
 
         owiLookup[r.result.idOwi[0]].forEach((i) => {
           if (parent.result['@id'] !== i.result['@id']) {
-            parent.collapsedBibs.push(i);
+            parent.collapsedBibs.push(i)
           }
-        });
-        newItemListElement.push(parent);
+        })
+        newItemListElement.push(parent)
       } else {
         // something went wrong, just add them all in
         owiLookup[r.result.idOwi[0]].forEach((i) => {
-          newItemListElement.push(i);
+          newItemListElement.push(i)
         });
       }
     } else {
-      newItemListElement.push(r);
+      newItemListElement.push(r)
     }
   });
 
-  results.searchResults.itemListElement = newItemListElement;
+  results.searchResults.itemListElement = newItemListElement
   return results;
 }
 
