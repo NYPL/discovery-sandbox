2331b47b8c1dd9089dc92440a529b2d74de649ff
Ashley Blewer <ashley.blewer@gmail.com>
2017-07-11 15:25:31 -0400

    pulls in code for a new search

--- src/app/components/BibPage/BibMainInfo.jsx
+++ src/app/components/BibPage/BibMainInfo.jsx
@@ -6,6 +6,8 @@ import {
   isEmpty as _isEmpty,
 } from 'underscore';
 import DefinitionList from './DefinitionList';
+import { ajaxCall } from '../../utils/utils';
+import Actions from '../../actions/Actions';
 
 class BibMainInfo extends React.Component {
 
@@ -47,6 +49,51 @@ class BibMainInfo extends React.Component {
           </span>
         ),
       };
+    });
+  }
+
+  newSearch(e, query) {
+    e.preventDefault();
+
+    Actions.updateSpinner(true);
+    ajaxCall(`/api?${query}`, (response) => {
+      const closingBracketIndex = query.indexOf(']');
+      const equalIndex = query.indexOf('=') + 1;
+
+      const field = query.substring(8, closingBracketIndex);
+      const value = query.substring(equalIndex);
+
+      // Find the index where the field exists in the list of facets from the API
+      const index = _findIndex(response.data.facets.itemListElement, { field });
+
+      // If the index exists, try to find the facet value from the API
+      if (response.data.facets.itemListElement[index]) {
+        const facet = _findWhere(response.data.facets.itemListElement[index].values, { value });
+
+        // The API may return a list of facets in the selected field, but the wanted
+        // facet may still not appear. If that's the case, return the clicked facet value.
+        Actions.updateSelectedFacets({
+          [field]: [{
+            id: facet ? facet.value : value,
+            value: facet ? (facet.label || facet.value) : value,
+          }],
+        });
+      } else {
+        // Otherwise, the field wasn't found in the API. Returning this highlights the
+        // facet in the selected facet region, but not in the facet sidebar.
+        Actions.updateSelectedFacets({
+          [field]: [{
+            id: value,
+            value,
+          }],
+        });
+      }
+      Actions.updateSearchResults(response.data.searchResults);
+      Actions.updateFacets(response.data.facets);
+      Actions.updateSearchKeywords('');
+      Actions.updatePage('1');
+      this.context.router.push(`/search?${query}`);
+      Actions.updateSpinner(false);
     });
   }
 
