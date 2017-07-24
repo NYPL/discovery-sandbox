909f0db1f0f0527145c1859aa37f9d71a0c7129e
Edwin Guzman <edwin.gzmn@gmail.com>
2017-07-24 11:22:23 -0400

    Removing redirect from ajax function.

--- server.js
+++ server.js
@@ -49,14 +49,6 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true,
 }));
 
-// Enable CORS
-app.use('/*', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested With, Content-Type, Accept');
-  next();
-});
-
 // Set Global publicKey
 app.set('nyplPublicKey', appConfig.publicKey);
 
--- src/server/ApiRoutes/Hold.js
+++ src/server/ApiRoutes/Hold.js
@@ -249,12 +249,12 @@ function newHoldRequestServer(req, res, next) {
  */
 function newHoldRequestAjax(req, res) {
   const bibId = req.params.bibId || '';
-  const loggedIn = User.requireUser(req, res);
+  // const loggedIn = User.requireUser(req, res);
 
-  if (!loggedIn) return false;
+  // if (!loggedIn) return false;
 
   const accessToken = req.tokenResponse.accessToken || '';
-  const patronId = req.tokenResponse.decodedPatron.sub || '';
+  const patronId = req.tokenResponse.decodedPatron ? req.tokenResponse.decodedPatron.sub : '';
   let barcode;
 
   // Retrieve item
