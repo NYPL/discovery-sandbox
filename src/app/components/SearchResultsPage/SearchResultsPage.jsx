9747231d6ccedbb4414b6f79bc1a0f8ecf55c009
Kang Peng <kangtingpeng@nypl.org>
2017-08-18 15:50:36 -0400

    Updated SearchResultsPage.jsx to have state.

--- src/app/components/SearchResultsPage/SearchResultsPage.jsx
+++ src/app/components/SearchResultsPage/SearchResultsPage.jsx
@@ -8,6 +8,7 @@ import ResultList from '../Results/ResultsList';
 import Search from '../Search/Search.jsx';
 import Sorter from '../Sorter/Sorter';
 import Pagination from '../Pagination/Pagination';
+import LoadingLayer from '../LoadingLayer/LoadingLayer.jsx';
 
 import {
   basicQuery,
@@ -16,119 +17,136 @@ import {
 import Actions from '../../actions/Actions.js';
 import appConfig from '../../../../appConfig.js';
 
-const SearchResultsPage = (props, context) => {
-  const {
-    searchResults,
-    searchKeywords,
-    selectedFacets,
-    page,
-    sortBy,
-    field,
-    spinning,
-  } = props;
+class SearchResultsPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const totalResults = searchResults ? searchResults.totalResults : undefined;
-  const totalPages = totalResults ? Math.floor(totalResults / 50) + 1 : 0;
-  const results = searchResults ? searchResults.itemListElement : [];
-  const createAPIQuery = basicQuery(props);
-  const searchKeywordsLabel = searchKeywords ? `for ${searchKeywords}` : '';
-  const pageLabel = totalPages ? `page ${page} of ${totalPages}` : '';
-  const headerLabel = `Search results ${searchKeywordsLabel} ${pageLabel}`;
-  const updatePage = (nextPage) => {
-    Actions.updateSpinner(true);
-    // Temporary. Need to check cross-browser and if it's needed at all.
-    window.scrollTo(0, 0);
-    const apiQuery = createAPIQuery({ page: nextPage });
+    this.state = {
+      isDiscoverying: this.props.isDiscoverying,
+    };
 
-    ajaxCall(`${appConfig.baseUrl}/api?${apiQuery}`, response => {
-      Actions.updateSearchResults(response.data.searchResults);
-      Actions.updatePage(nextPage.toString());
-      Actions.updateSpinner(false);
-      context.router.push(`${appConfig.baseUrl}/search?${apiQuery}`);
-    });
-  };
+    this.updateIsDiscoveryingState = this.updateIsDiscoveryingState.bind(this);
+  }
 
-  return (
-    <DocumentTitle
-      title="Search Results | Shared Collection Catalog | NYPL"
-    >
-      <main className="main-page">
-        <div className="nypl-page-header">
+  updateIsDiscoveryingState(status) {
+    this.setState({ isDiscoverying: status });
+  }
+
+  render() {
+    const {
+      searchResults,
+      searchKeywords,
+      selectedFacets,
+      page,
+      sortBy,
+      field,
+      spinning,
+    } = this.props;
+
+    const totalResults = searchResults ? searchResults.totalResults : undefined;
+    const totalPages = totalResults ? Math.floor(totalResults / 50) + 1 : 0;
+    const results = searchResults ? searchResults.itemListElement : [];
+    const createAPIQuery = basicQuery(this.props);
+    const searchKeywordsLabel = searchKeywords ? `for ${searchKeywords}` : '';
+    const pageLabel = totalPages ? `page ${page} of ${totalPages}` : '';
+    const headerLabel = `Search results ${searchKeywordsLabel} ${pageLabel}`;
+    const updatePage = (nextPage) => {
+      Actions.updateDiscoveryingStatus(true);
+      // Temporary. Need to check cross-browser and if it's needed at all.
+      window.scrollTo(0, 0);
+      const apiQuery = createAPIQuery({ page: nextPage });
+
+      ajaxCall(`${appConfig.baseUrl}/api?${apiQuery}`, response => {
+        Actions.updateSearchResults(response.data.searchResults);
+        Actions.updatePage(nextPage.toString());
+        Actions.updateDiscoveryingStatus(false);
+        context.router.push(`${appConfig.baseUrl}/search?${apiQuery}`);
+      });
+    };
+
+    return (
+      <DocumentTitle
+        title="Search Results | Shared Collection Catalog | NYPL"
+      >
+        <main className="main-page">
+          <LoadingLayer status={this.state.isDiscoverying} title="Searching" />
+          <div className="nypl-page-header">
+            <div className="nypl-full-width-wrapper">
+              <div className="nypl-row">
+                <div className="nypl-column-three-quarters">
+                  <Breadcrumbs query={searchKeywords} type="search" />
+                  <h2 aria-label={headerLabel}>{appConfig.displayTitle}</h2>
+                  <Search
+                    searchKeywords={searchKeywords}
+                    field={field}
+                    createAPIQuery={createAPIQuery}
+                    updateIsDiscoveryingState={this.updateIsDiscoveryingState}
+                  />
+
+                  {
+                    !!(totalResults && totalResults !== 0) && (
+                      <Sorter
+                        sortBy={sortBy}
+                        page={page}
+                        searchKeywords={searchKeywords}
+                        createAPIQuery={createAPIQuery}
+                      />
+                    )
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div className="nypl-full-width-wrapper">
             <div className="nypl-row">
-              <div className="nypl-column-three-quarters">
-                <Breadcrumbs query={searchKeywords} type="search" />
-                <h2 aria-label={headerLabel}>{appConfig.displayTitle}</h2>
-                <Search
+              <div
+                className="nypl-column-three-quarters"
+                role="region"
+                id="mainContent"
+                aria-live="polite"
+                aria-atomic="true"
+                aria-relevant="additions removals"
+                aria-describedby="results-description"
+              >
+                <ResultsCount
+                  spinning={spinning}
+                  count={totalResults}
+                  selectedFacets={selectedFacets}
                   searchKeywords={searchKeywords}
                   field={field}
-                  spinning={spinning}
-                  createAPIQuery={createAPIQuery}
+                  page={parseInt(page, 10)}
                 />
 
                 {
-                  !!(totalResults && totalResults !== 0) && (
-                    <Sorter
-                      sortBy={sortBy}
-                      page={page}
+                  !!(results && results.length !== 0) &&
+                  (
+                    <ResultList
+                      results={results}
+                      spinning={spinning}
                       searchKeywords={searchKeywords}
-                      createAPIQuery={createAPIQuery}
                     />
                   )
+                }
+
+                {
+                  !!(totalResults && totalResults !== 0) &&
+                    (<Pagination
+                      ariaControls="nypl-results-list"
+                      total={totalResults}
+                      perPage={50}
+                      page={parseInt(page, 10)}
+                      createAPIQuery={createAPIQuery}
+                      updatePage={updatePage}
+                    />)
                 }
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="nypl-full-width-wrapper">
-          <div className="nypl-row">
-            <div
-              className="nypl-column-three-quarters"
-              role="region"
-              id="mainContent"
-              aria-live="polite"
-              aria-atomic="true"
-              aria-relevant="additions removals"
-              aria-describedby="results-description"
-            >
-              <ResultsCount
-                spinning={spinning}
-                count={totalResults}
-                selectedFacets={selectedFacets}
-                searchKeywords={searchKeywords}
-                field={field}
-                page={parseInt(page, 10)}
-              />
-
-              {
-                !!(results && results.length !== 0) &&
-                (
-                  <ResultList
-                    results={results}
-                    spinning={spinning}
-                    searchKeywords={searchKeywords}
-                  />
-                )
-              }
-
-              {
-                !!(totalResults && totalResults !== 0) &&
-                  (<Pagination
-                    ariaControls="nypl-results-list"
-                    total={totalResults}
-                    perPage={50}
-                    page={parseInt(page, 10)}
-                    createAPIQuery={createAPIQuery}
-                    updatePage={updatePage}
-                  />)
-              }
-            </div>
-          </div>
-        </div>
-      </main>
-    </DocumentTitle>
-  );
+        </main>
+      </DocumentTitle>
+    );
+  }
 };
 
 SearchResultsPage.propTypes = {
