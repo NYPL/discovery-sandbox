9d33b65f3aaf1d974ecd6c93d32e0337d9b1c051
Kang Peng <kangtingpeng@nypl.org>
2017-09-14 15:15:04 -0400

    Updated the check for end year later than start year.

--- src/app/components/Filters/FieldsetDate.jsx
+++ src/app/components/Filters/FieldsetDate.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class FilterPopup extends React.Component {
+class FieldsetDate extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      startDateInput: '',
-      endDateInput: '',
+      startDateInput: 0,
+      endDateInput: 0,
     };
+
+    this.inputChange = this.inputChange.bind(this);
+  }
+
+  /*
+   * inputChange
+   */
+  inputChange(e) {
+    const value = (e.target.value).replace(/[a-zA-Z]/g, '');
+    let displayValue = '';
+
+    if (e.target.name === 'start-date') {
+      displayValue = 'startDateInput';
+    } else if (e.target.name === 'end-date') {
+      displayValue = 'endDateInput';
+    }
+
+    if (displayValue) {
+      this.setState({
+        [displayValue]: value
+      });
+    }
   }
 
   render() {
+    let errorMessage = '';
+
+    if (this.state.endDateInput && this.state.startDateInput) {
+      if (this.state.endDateInput < this.state.startDateInput) {
+        errorMessage = 'end year should be later than start year.';
+      }
+    } else {
+      errorMessage = '';
+    }
+
     return (
       <fieldset>
         <legend>Date</legend>
@@ -21,8 +53,9 @@ class FilterPopup extends React.Component {
               id="start-date"
               name="start-date"
               className="form-text"
-              type="number"
-              max="9999"
+              type="text"
+              maxLength="4"
+              onChange={this.inputChange}
             />
           </label>
           <label htmlFor="end-date">End Year
@@ -30,19 +63,21 @@ class FilterPopup extends React.Component {
               id="end-date"
               name="end-date"
               className="form-text"
-              type="number"
-              max="9999"
+              type="text"
+              maxLength="4"
+              onChange={this.inputChange}
             />
           </label>
           <span>The Start year cannot be later than the end year</span>
+          <br />
+          <span>{errorMessage}</span>
         </div>
       </fieldset>
     );
   }
 }
 
-FilterPopup.propTypes = {
-  location: PropTypes.object,
+FieldsetDate.propTypes = {
 };
 
-export default FilterPopup;
+export default FieldsetDate;
