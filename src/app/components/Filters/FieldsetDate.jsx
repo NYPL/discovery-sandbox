15b0624520dfea7dd663b4769af5426fbf16b7f2
Kang Peng <kangtingpeng@nypl.org>
2017-09-14 12:17:59 -0400

    Added pattern to control the length of year and other factor input.

--- src/app/components/Filters/FieldsetDate.jsx
+++ src/app/components/Filters/FieldsetDate.jsx
@@ -2,16 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class FilterPopup extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
   render() {
     return (
       <fieldset>
         <legend>Date</legend>
         <div>
           <label htmlFor="start-date">Start Year
-            <input id="start-date" name="start-date" className="form-text" type="date" />
+            <input
+              id="start-date"
+              name="start-date"
+              className="form-text"
+              type="date"
+              pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+            />
           </label>
           <label htmlFor="end-date">End Year
-            <input id="end-date" type="date" className="form-text" name="end-date" />
+            <input
+              id="end-date"
+              type="date"
+              className="form-text"
+              name="end-date"
+              pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
+            />
           </label>
           <span>The Start year cannot be later than the end year</span>
         </div>
