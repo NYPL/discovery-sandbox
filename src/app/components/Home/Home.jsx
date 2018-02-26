0fbecfce7ad64afba53f4841115965455a3ff7e7
Edwin Guzman <edwin.gzmn@gmail.com>
2018-02-26 17:39:27 -0500

    Updating the design toolkit, removing scss files, and updating dom classes.

--- src/app/components/Home/Home.jsx
+++ src/app/components/Home/Home.jsx
@@ -45,6 +45,11 @@ class Home extends React.Component {
               <div className="nypl-row">
                 <div className="nypl-column-full">
                   <h1>{appConfig.displayTitle}</h1>
+                </div>
+              </div>
+
+              <div className="nypl-row">
+                <div className="nypl-column-full">
                   <Search
                     createAPIQuery={basicQuery(this.props)}
                     updateIsLoadingState={this.updateIsLoadingState}
