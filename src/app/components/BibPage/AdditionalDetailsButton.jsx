e213392df0939419cd65da1fe497c4bf2818c450
Daniel Appel <dan.s.appel@gmail.com>
2018-05-16 13:39:29 -0400

    compactify AdditionalDetailsButton

--- src/app/components/BibPage/AdditionalDetailsButton.jsx
+++ src/app/components/BibPage/AdditionalDetailsButton.jsx
@@ -6,23 +6,38 @@ class AdditionalDetailsButton extends React.Component {
     this.state = {
       display: false
     }
+    this.toggleState = this.toggleState.bind(this);
   }
 
+  toggleState(){
+    this.setState({display: !this.state.display});
+  }
+
+  // render(){
+  //     if(this.state.display){
+  //       return(
+  //         <div>
+  //           <button onClick={() => this.setState({display:false})}>Close Details</button>
+  //           <p> Additional Details Will Go Here </p>
+  //         </div>
+  //       )
+  //     }else{
+  //       return (
+  //         <button onClick={() => this.setState({display:true})}>
+  //         See More Details
+  //         </button>
+  //       )
+  //     }
+  // }
+
   render(){
-      if(this.state.display){
-        return(
-          <div>
-            <button onClick={() => this.setState({display:false})}>Close Details</button>
-            <p> Additional Details Will Go Here </p>
-          </div>
-        )
-      }else{
-        return (
-          <button onClick={() => this.setState({display:true})}>
-          See More Details
-          </button>
-        )
-      }
+    return(
+      <div>
+      <button onClick = {this.toggleState}>{this.state.display ? "Close Details" : "See More Details"}</button>
+      <br/>
+      {this.state.display ? "Additional Details Will Go Here" : null}
+      </div>
+    )
   }
 }
 
