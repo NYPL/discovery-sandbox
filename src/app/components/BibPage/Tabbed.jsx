f02729d7d1a59a3ddf87646c1f8ee8e85c51adbb
Daniel Appel <dan.s.appel@gmail.com>
2018-06-05 16:13:26 -0400

    Add tabIndex to focus on panel. At this point all functionality should be present

--- src/app/components/BibPage/Tabbed.jsx
+++ src/app/components/BibPage/Tabbed.jsx
@@ -5,36 +5,41 @@ class Tabbed extends React.Component {
     super(props);
 
     this.state = {
-      tabNumber: 0
+      tabNumber: 0,
       numberOfTabs: this.props.tabs.length
     };
 
     this.switchTab = this.switchTab.bind(this);
     this.clickHandler = this.clickHandler.bind(this);
+    this.keyDownHandler = this.keyDownHandler.bind(this);
   }
 
-  switchTab (newTab) {
-    const index  = parseInt(newTab.getAttribute('data'));
-    this.setState({ tabNumber: index }); //prop vs attribute
+  switchTab (newTabIndex) {
+    // const index  = parseInt(newTab.getAttribute('data'));
+    this.setState({ tabNumber: newTabIndex }); //prop vs attribute
+    let newTab = document.getElementById(newTabIndex);
     newTab.focus(); //this may need to be changed because of re-rendering and synchronicity issues
   }
 
   clickHandler (e) {
     e.preventDefault();
-    if (e.currentTarget.getAttribute('data') !== this.state.tabNumber.toString()) {
-      this.switchTab(e.currentTarget);
+    let clickedTab = e.currentTarget;
+    let index = parseInt(clickedTab.getAttribute('data'));
+    if (index !== this.state.tabNumber) {
+      this.switchTab(index);
     }
   }
 
-  // keyDownHandler (e) {
-  //   const panel = document.getElementById('panel').children[0];
-  //   const index = parseInt(e.currentTarget.getAttribute('data'));
-  //   let dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
-  //   if (dir !== null) {
-  //     e.preventDefault();
-  //     dir ===  'down' ? panel.focus() : dir < this.numberOfTabs ? this.switchTab(e.currentTarget)
-  //   }
-  // }
+  keyDownHandler (e) {
+    const panel = document.getElementById('panel');//.children[0];
+    const index = parseInt(e.currentTarget.getAttribute('data'));
+    let dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
+    debugger
+    if (dir !== null) {
+      e.preventDefault();
+      dir ===  'down' ? panel.focus() : dir < this.state.numberOfTabs ? this.switchTab(dir) : void 0;
+    }
+  }
 
 
   render() {
@@ -42,15 +47,16 @@ class Tabbed extends React.Component {
       <h3></h3>
       <ul role='tablist'>
         { this.props.tabs.map((tab, i) => {
-          return (<li role='presentation'>
+          return (<li role='presentation' key={i}>
               <a href={`#section${i}`} tabIndex={ i === this.state.tabNumber ? null : -1 }
-               aria-selected={ i === this.state.tabNumber ? true : false} role='tab' data={i} onClick={this.clickHandler}>{tab.title}</a>
+               aria-selected={ i === this.state.tabNumber ? true : false} role='tab' data={i} id={i}
+               onClick={this.clickHandler} onKeyDown={this.keyDownHandler} > {tab.title} </a>
             </li>);
         })}
       </ul>
-      <div id="panel">
+      <section id="panel" tabIndex="0">
       { this.props.tabs[this.state.tabNumber].content }
-      </div>
+      </section>
     </div>)
   }
 }
