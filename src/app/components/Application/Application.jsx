9694acd8fdb382608aaff38ea8cfd33b54267ab9
Daniel Appel <dan.s.appel@gmail.com>
2019-05-03 12:01:05 -0400

    Fix too many redirects bug

--- src/app/components/Application/Application.jsx
+++ src/app/components/Application/Application.jsx
@@ -39,7 +39,8 @@ class App extends React.Component {
   componentDidMount() {
     Store.listen(this.onChange);
     // Listen to the browser's navigation buttons.
-    window.appHistory.listen((location) => {
+    // console.log(this.props.route)
+    this.props.route.history.listen((location) => {
       const {
         action,
         search,
