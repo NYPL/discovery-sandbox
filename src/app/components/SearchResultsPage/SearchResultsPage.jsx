d722f5f9afffc9ae09b0981327ad7d1f6706d371
Daniel Appel <dan.s.appel@gmail.com>
2019-05-06 10:41:08 -0400

    Refactor narrowSubjectFilters for greater readability

--- src/app/components/SearchResultsPage/SearchResultsPage.jsx
+++ src/app/components/SearchResultsPage/SearchResultsPage.jsx
@@ -81,23 +81,28 @@ class SearchResultsPage extends React.Component {
     return false;
   }
 
-  narrowSubjectFilters(apiFilters, selectedFilters) {
+  getSubjectLiteralFilters(apiFilters) {
     const apiSubjectLiteralFilters = apiFilters.filter(
       apiFilter => apiFilter.field === 'subjectLiteral',
     );
-    if (apiSubjectLiteralFilters.length) {
-      const subjectLiteralFilters = apiSubjectLiteralFilters[0];
+    return apiSubjectLiteralFilters.length ? apiSubjectLiteralFilters[0] : null;
+  }
+
+  subjectFilterIsSelected(selectedSubjectLiteralFilters) {
+    return subjectLiteralFilter =>
+      selectedSubjectLiteralFilters.some(
+        selectedFilter => subjectLiteralFilter.value === selectedFilter.value,
+      );
+  }
+
+  narrowSubjectFilters(apiFilters, selectedFilters) {
+    const subjectLiteralFilters = this.getSubjectLiteralFilters(apiFilters);
+    const selectedSubjectLiteralFilters = selectedFilters.subjectLiteral;
+    const checkIsSelected = this.subjectFilterIsSelected(selectedSubjectLiteralFilters);
+    if (subjectLiteralFilters) {
       subjectLiteralFilters.values = subjectLiteralFilters
         .values
-        .filter(
-          subjectLiteralFilter => (
-            selectedFilters
-              .subjectLiteral
-              .some(selectedFilter =>
-                subjectLiteralFilter.value === selectedFilter.value,
-              )
-          ),
-        );
+        .filter(checkIsSelected);
     }
   }
 
