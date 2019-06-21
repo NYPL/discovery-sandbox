28157603b38b7f806ec1d166ca8a988a8facf0b1
Daniel Appel <dan.s.appel@gmail.com>
2019-06-21 16:41:05 -0400

    Add logic to replace subject filters with selected filters

--- src/app/utils/subjectFilterUtils.js
+++ src/app/utils/subjectFilterUtils.js
@@ -14,86 +14,19 @@ const subjectFilterUtil = {
   },
 
   /**
-    params: subjectFilterObject is an object representing a subject filter, e.g.
-    {
-      value: 'Dogs -- Painting -- Smoking',
-      label: 'Dogs -- Painting -- Smoking',
-      count: 100000000,
-    }
-    returns an array of exploded values, e.g.
-    ['Dogs', 'Dogs -- Painting', 'Dogs -- Painting -- Smoking']
-  */
-
-  explodeSubjectFilter(subjectFilterObject) {
-    const explodedValues = subjectFilterObject
-      .value
-      .replace(/\.$/, '')
-      .split(/--/g);
-    return explodedValues.map((_, i) => explodedValues.slice(0, i + 1).join('--').trim());
-  },
-
-  /**
-    params: selectedSubjectLiteralFilters is an object with a 'values' property, which
-    points to an array of filters represented by objects of the form:
-    {
-      value,
-      label,
-      count
-    }
-    e.g. the value could be 'Dogs -- Card Games -- Painting.'
-
-    explodedSubjectFilters modifies the array by replacing the values with exploded
-    versions of those values. Exploded values have no 'count' property.
-  */
-
-  explodeSubjectFilters(selectedSubjectLiteralFilters) {
-    let explodedSubjectFilters = selectedSubjectLiteralFilters
-      .values
-      .reduce((acc, subjectFilterObject) => {
-        this.explodeSubjectFilter(subjectFilterObject)
-          .forEach((explodedSubjectFilter) => {
-            acc.add(explodedSubjectFilter);
-          });
-        return acc;
-      }, new Set());
-    explodedSubjectFilters = Array.from(explodedSubjectFilters)
-      .map(explodedSubjectFilter => (
-        {
-          value: explodedSubjectFilter,
-          label: explodedSubjectFilter,
-        }
-      ),
-      );
-    selectedSubjectLiteralFilters.values = explodedSubjectFilters;
-  },
-
-  /**
     params:
       apiFilters: an object containing all the aggregations received from the api
       selectedFilters: an object containing the currently selected filters (selected in
          the Refine Search accordion)
     narrowSubjectFilters returns a new object, which is a copy of apiFilters but with only
-     the selected subjectLiteralFilters
+     the selected subjectFilters from selectedFilters
   */
-
   narrowSubjectFilters(apiFilters, selectedFilters) {
-    // deep copy the apiFilters object
     const newApiFilters = JSON.parse(JSON.stringify(apiFilters));
-    // grab the aggregated subject literal filters
     const subjectLiteralFilters = this.getSubjectLiteralFilters(newApiFilters);
-    // add the exploded subject literals if there are any
-    if (subjectLiteralFilters) {
-      this.explodeSubjectFilters(subjectLiteralFilters);
-    }
-    // get the selected subject literals
     const selectedSubjectLiteralFilters = selectedFilters.subjectLiteral || [];
-    // build a function which filters subject filters down to those in selectedSubjectLiteralFilters
-    const checkIsSelected = this.subjectFilterIsSelected(selectedSubjectLiteralFilters);
-    // remove all the unselected filters from subject literal filters
     if (subjectLiteralFilters) {
-      subjectLiteralFilters.values = subjectLiteralFilters
-        .values
-        .filter(checkIsSelected);
+      subjectLiteralFilters.values = selectedSubjectLiteralFilters;
     }
     return newApiFilters;
   },
