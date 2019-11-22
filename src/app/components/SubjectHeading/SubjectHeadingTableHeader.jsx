5f53b1a42edb55e2e6f4b48c33eb93f11ccbf813
Serena Berry <serenaberry@nypl.org>
2019-11-22 11:47:40 -0500

    table heading HTML structure

--- src/app/components/SubjectHeading/SubjectHeadingTableHeader.jsx
+++ src/app/components/SubjectHeading/SubjectHeadingTableHeader.jsx
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types';
 const SubjectHeadingTableHeader = () => {
   return (
       <div className="subjectHeadingRow tableHeadings">
-        <span className="subjectHeadingLabelAndToggle">
-          <span className="subjectHeadingToggle"></span>
+      <span className="subjectHeadingToggle"></span>
+        <span className="subjectHeadingLabelContainer">
           <span className="subjectHeadingLabel">Subject Heading</span>
         </span>
         <span className="subjectHeadingAttribute titles">Titles</span>
