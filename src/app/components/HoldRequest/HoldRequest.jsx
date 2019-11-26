a9c197a00417e53c18c7c37f348c421cd998c689
Serena Berry <serenaberry@nypl.org>
2019-11-26 16:41:52 -0500

    Make more general and set for Thanksgiving holiday dates

--- src/app/components/HoldRequest/HoldRequest.jsx
+++ src/app/components/HoldRequest/HoldRequest.jsx
@@ -87,21 +87,18 @@ class HoldRequest extends React.Component {
    */
   getNotification() {
     // Show holiday schedule warning if current date strictly less than
-    // 2019-01-02 (i.e. don't show notification after 2019-01-01 23:59:59)
-    const is2019Jan2OrLater = new Date() >= new Date(2019, 0, 2)
-    if (is2019Jan2OrLater) {
-      return null;
-    }
+    // the date the holiday hours end
+    const isAlertRelevant = new Date() <= new Date(2019, 11, 2)
 
-    return (
-      <div className="nypl-banner-alert">
-        <p style={{ padding: '10px 20px 0px', margin: 0 }}>
-          Deliveries of offsite requests are subject to holiday scheduling. Please check 
-          that your items are labeled "Ready for Pickup" in your account in advance of 
-          your visit.
-        </p>
-      </div>
-    );
+    if (isAlertRelevant) {
+      return (
+        <div className="nypl-banner-alert">
+          <p style={{ padding: '10px 20px 0px', margin: 0 }}>
+            Please note that due to the holiday schedule, requests for offsite material placed between 2:30pm on November 26 and 2:30pm on Monday, December 2 will be delivered on Tueday, December 3.
+          </p>
+        </div>
+      );
+    }
   }
 
   /**
@@ -299,7 +296,7 @@ class HoldRequest extends React.Component {
       </label>
     );
   }
-  
+
   render() {
     const searchKeywords = this.props.searchKeywords || '';
     const bib = (this.props.bib && !_isEmpty(this.props.bib)) ?
