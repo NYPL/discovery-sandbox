1568cb6086cd80f55891cc2bfecfbc81dc1f7843
Daniel Appel <dan.s.appel@gmail.com>
2019-12-23 16:16:15 -0500

    Change preview component to tr

--- src/app/components/SubjectHeading/PreviewComponents.jsx
+++ src/app/components/SubjectHeading/PreviewComponents.jsx
@@ -7,16 +7,18 @@ const Preview = (props) => {
   const { topHeadings } = props;
   const groupedHeadings = [[topHeadings[0], topHeadings[2]], [topHeadings[1], topHeadings[3]]];
   return (
-    <div className="preview subjectHeadingRow" colSpan={4}>
-      <div className="previewDiv">
-        <em>Most common subheadings:</em>
-        <div className="previewInner">
-          <ul className="previewUl">
-            {groupedHeadings.map(headings => <PreviewCol col={headings} />)}
-          </ul>
+    <tr className="preview subjectHeadingRow">
+      <td colSpan="4">
+        <div className="previewDiv">
+          <em>Most common subheadings:</em>
+          <div className="previewInner">
+            <ul className="previewUl">
+              {groupedHeadings.map(headings => <PreviewCol col={headings} />)}
+            </ul>
+          </div>
         </div>
-      </div>
-    </div>
+      </td>
+    </tr>
   );
 };
 
