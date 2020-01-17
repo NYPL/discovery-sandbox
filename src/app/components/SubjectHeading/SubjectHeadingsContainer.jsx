0499fd0b32556d9a6ee8489ae4a9429a6dd5e590
Daniel Appel <dan.s.appel@gmail.com>
2020-01-17 14:20:06 -0500

    Remove console logs

--- src/app/components/SubjectHeading/SubjectHeadingsContainer.jsx
+++ src/app/components/SubjectHeading/SubjectHeadingsContainer.jsx
@@ -27,7 +27,6 @@ class SubjectHeadingsContainer extends React.Component {
   }
 
   componentDidMount() {
-    console.log('mounting')
     let {
       fromLabel,
       fromComparator,
@@ -159,8 +158,6 @@ class SubjectHeadingsContainer extends React.Component {
         : null
     );
 
-
-    console.log('rendering', this.props.location.query);
     return (
       <div
         className="nypl-column-full subjectHeadingMainContent index"
