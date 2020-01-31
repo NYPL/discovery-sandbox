1f132545d744cc8ead476aea1c18a9c495a30fa9
Serena Berry <serenaberry@nypl.org>
2020-01-31 15:44:39 -0500

    resimplify shep TableHeader

--- src/app/components/SubjectHeading/SubjectHeadingsTableHeader.jsx
+++ src/app/components/SubjectHeading/SubjectHeadingsTableHeader.jsx
@@ -1,30 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SubjectHeadingsTableHeader = () => {
-  return (
-      <thead>
-        <tr>
-          <th className="subjectHeadingsTableCell subjectHeadingLabel">
-            <div className="subjectHeadingToggle"></div>
-            <div className="subjectHeadingLabelInner">Heading</div>
-          </th>
-          <th className="subjectHeadingsTableCell subjectHeadingAttribute titles">Title Count</th>
-          <th className="subjectHeadingsTableCell subjectHeadingAttribute narrower">Subheading Count</th>
-        </tr>
-      </thead>
-  );
-};
-
-SubjectHeadingsTableHeader.propTypes = {
-  index: PropTypes.bool,
-  revealSortColumn: PropTypes.bool,
-};
-
-SubjectHeadingsTableHeader.defaultProps = {
-  // safer to not have an extra column or column heading
-  index: false,
-  revealSortColumn: false,
-};
+const SubjectHeadingsTableHeader = () => (
+    <thead>
+      <tr>
+        <th className="subjectHeadingsTableCell subjectHeadingLabel">
+          <div className="subjectHeadingToggle"/>
+          <div className="subjectHeadingLabelInner">Heading</div>
+        </th>
+        <th className="subjectHeadingsTableCell subjectHeadingAttribute titles">Title Count</th>
+        <th className="subjectHeadingsTableCell subjectHeadingAttribute narrower">Subheading Count</th>
+      </tr>
+    </thead>
+);
 
 export default SubjectHeadingsTableHeader;
