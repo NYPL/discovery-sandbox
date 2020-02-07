55a8fe01a1de6a2c97a7e40e831bf933feeff563
Serena Berry <serenaberry@nypl.org>
2020-02-07 12:15:22 -0500

    remove superfluous sortButton

--- src/app/components/SubjectHeading/SubjectHeadingsContainer.jsx
+++ src/app/components/SubjectHeading/SubjectHeadingsContainer.jsx
@@ -159,7 +159,6 @@ class SubjectHeadingsContainer extends React.Component {
       <React.Fragment>
         {this.pagination()}
         {filter ? null : <AlphabeticalPagination />}
-        {sortButton}
         <SubjectHeadingsTable
           index
           subjectHeadings={subjectHeadings}
