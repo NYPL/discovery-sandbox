082092b519df1897ec4749db36f6a3e63936f729
Serena Berry <serenaberry@nypl.org>
2020-06-10 17:11:45 -0400

    assign proper download link

--- src/app/components/Drbb/DrbbItem.jsx
+++ src/app/components/Drbb/DrbbItem.jsx
@@ -69,6 +69,35 @@ const DrbbItem = (props) => {
       </Link>
     );
   };
+  let downloadLink;
+  let downloadItem;
+  const downloadableEdition = editions.find(workEdition => (
+    workEdition.items.find((editionItem) => {
+      downloadItem = editionItem;
+      return editionItem.links.find((link) => {
+        downloadLink = link;
+        return link.download;
+      });
+    })
+  ));
+
+  const downloadLinkElement = () => {
+    if (!downloadLink) return null;
+
+    return (
+      <Link
+        target="_blank"
+        to={formatUrl(downloadLink.url)}
+        className="drbb-download-pdf"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+          <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 15.3C4 15.3.7 12 .7 8S4 .7 8 .7 15.3 4 15.3 8 12 15.3 8 15.3z" />
+          <path d="M11.3 8.2c-.2-.2-.5-.2-.7 0l-2.2 2.2V4.5h-.9v5.9L5.3 8.2c-.1-.2-.4-.2-.6 0s-.2.5 0 .7l3 3c.2.2.5.2.7 0l3-3c.1-.2.1-.5-.1-.7z" />
+        </svg>
+        Download PDF
+      </Link>
+    );
+  };
 
   return (
     <li className="drbb-item">
@@ -81,17 +110,7 @@ const DrbbItem = (props) => {
       </Link>
       {agents && agents.length ? authorship() : null}
       { readOnlineLink() }
-      <Link
-        target="_blank"
-        to={`${drbbFrontEnd}/work?recordType=editions&workId=${work.uuid}`}
-        className="drbb-download-pdf"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-          <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 15.3C4 15.3.7 12 .7 8S4 .7 8 .7 15.3 4 15.3 8 12 15.3 8 15.3z" />
-          <path d="M11.3 8.2c-.2-.2-.5-.2-.7 0l-2.2 2.2V4.5h-.9v5.9L5.3 8.2c-.1-.2-.4-.2-.6 0s-.2.5 0 .7l3 3c.2.2.5.2.7 0l3-3c.1-.2.1-.5-.1-.7z" />
-        </svg>
-        Download PDF
-      </Link>
+      { downloadLinkElement() }
     </li>
   );
 };
