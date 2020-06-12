13ef0bcbcd044c30d758f35fddce2e078a8401e4
Serena Berry <serenaberry@nypl.org>
2020-06-12 10:47:07 -0400

    Asset management for promo and download icon

--- src/app/components/Drbb/DrbbItem.jsx
+++ src/app/components/Drbb/DrbbItem.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 
+import DownloadIcon from '../../../client/icons/Download';
+
 import appConfig from '../../data/appConfig';
 import {
   authorQuery,
@@ -91,10 +93,7 @@ const DrbbItem = (props) => {
         to={formatUrl(downloadLink.url)}
         className="drbb-download-pdf"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-          <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 15.3C4 15.3.7 12 .7 8S4 .7 8 .7 15.3 4 15.3 8 12 15.3 8 15.3z" />
-          <path d="M11.3 8.2c-.2-.2-.5-.2-.7 0l-2.2 2.2V4.5h-.9v5.9L5.3 8.2c-.1-.2-.4-.2-.6 0s-.2.5 0 .7l3 3c.2.2.5.2.7 0l3-3c.1-.2.1-.5-.1-.7z" />
-        </svg>
+        <DownloadIcon />
         Download PDF
       </Link>
     );
