992b3b1180575e05acbae4da496f0fe1a97caf27
Serena Berry <serenaberry@nypl.org>
2020-06-17 13:18:11 -0400

    Revert `SearchResultsContainer` to functional component

--- src/app/components/SearchResults/SearchResultsContainer.jsx
+++ src/app/components/SearchResults/SearchResultsContainer.jsx
@@ -13,16 +13,13 @@ import Actions from '../../actions/Actions';
 import appConfig from '../../data/appConfig';
 
 // Renders the ResultsList containing the search results and the Pagination component
-class SearchResultsContainer extends React.Component {
-  constructor(props) {
-    super();
-    this.includeDrbb = true;
-    this.createAPIQuery = basicQuery(props);
-  }
+const SearchResultsContainer = (props, context) => {
+  const includeDrbb = true;
+  const createAPIQuery = basicQuery(props);
 
-  updatePage(nextPage, pageType) {
+  const updatePage = (nextPage, pageType) => {
     Actions.updateLoadingStatus(true);
-    const apiQuery = this.createAPIQuery({ page: nextPage });
+    const apiQuery = createAPIQuery({ page: nextPage });
 
     trackDiscovery('Pagination - Search Results', `${pageType} - page ${nextPage}`);
     ajaxCall(`${appConfig.baseUrl}/api?${apiQuery}`, (response) => {
@@ -30,56 +27,54 @@ class SearchResultsContainer extends React.Component {
       Actions.updatePage(nextPage.toString());
       setTimeout(() => {
         Actions.updateLoadingStatus(false);
-        this.context.router.push(`${appConfig.baseUrl}/search?${apiQuery}`);
+        context.router.push(`${appConfig.baseUrl}/search?${apiQuery}`);
       }, 500);
     });
-  }
+  };
 
-  render() {
-    const {
-      searchResults,
-      searchKeywords,
-      page,
-    } = this.props;
-    const { media } = this.context;
+  const {
+    searchResults,
+    searchKeywords,
+    page,
+  } = props;
+  const { media } = context;
 
-    const totalResults = searchResults ? searchResults.totalResults : undefined;
-    const results = searchResults ? searchResults.itemListElement : [];
+  const totalResults = searchResults ? searchResults.totalResults : undefined;
+  const results = searchResults ? searchResults.itemListElement : [];
 
-    return (
-      <React.Fragment>
-        <div className="nypl-row">
-          <div
-            className="nypl-column-full"
-            role="region"
-            aria-describedby="results-description"
-          >
-            {
-              !!(results && results.length !== 0) &&
-              <ResultsList
-                results={results}
-                searchKeywords={searchKeywords}
-              />
-            }
-            { this.includeDrbb && media === 'desktop' ? <DrbbContainer /> : null}
-            {
-              !!(totalResults && totalResults !== 0) &&
-              <Pagination
-                ariaControls="nypl-results-list"
-                total={totalResults}
-                perPage={50}
-                page={parseInt(page, 10)}
-                createAPIQuery={this.createAPIQuery}
-                updatePage={this.updatePage}
-              />
-            }
-            { this.includeDrbb && ['tablet', 'mobile'].includes(media) ? <DrbbContainer /> : null}
-          </div>
+  return (
+    <React.Fragment>
+      <div className="nypl-row">
+        <div
+          className="nypl-column-full"
+          role="region"
+          aria-describedby="results-description"
+        >
+          {
+            !!(results && results.length !== 0) &&
+            <ResultsList
+              results={results}
+              searchKeywords={searchKeywords}
+            />
+          }
+          { includeDrbb && media === 'desktop' ? <DrbbContainer /> : null}
+          {
+            !!(totalResults && totalResults !== 0) &&
+            <Pagination
+              ariaControls="nypl-results-list"
+              total={totalResults}
+              perPage={50}
+              page={parseInt(page, 10)}
+              createAPIQuery={createAPIQuery}
+              updatePage={updatePage}
+            />
+          }
+          { includeDrbb && ['tablet', 'mobile'].includes(media) ? <DrbbContainer /> : null}
         </div>
-      </React.Fragment>
-    );
-  }
-}
+      </div>
+    </React.Fragment>
+  );
+};
 
 SearchResultsContainer.propTypes = {
   searchResults: PropTypes.object,
