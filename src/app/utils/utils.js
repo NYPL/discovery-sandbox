b8dc23ce10c28da1955706dc94d00776c4c040f4
Serena Berry <serenaberry@nypl.org>
2020-07-16 13:44:36 -0400

    Add comment about util

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -448,6 +448,11 @@ function displayContext({ searchKeywords, selectedFilters, field, count }) {
   return clauses.length ? `for ${clauses.join(' and ')}` : '';
 }
 
+/**
+ * extractFeatures
+ * Takes comma-delimited list of values as a string and returns array of strings.
+ * @param {string} featuresString The comma-delimited list of features.
+ */
 const extractFeatures = (featuresString) => {
   if (typeof featuresString !== 'string') return [];
   return featuresString.split(',').reduce((features, feature) => {
