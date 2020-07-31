4ef16092155d1cbc8a4bc0a8845dd959d8a18db2
Daniel Appel <dan.s.appel@gmail.com>
2020-07-31 16:40:09 -0400

    Add some tests for DataLoader

--- test/unit/DataLoader.test.js
+++ test/unit/DataLoader.test.js
@@ -5,24 +5,59 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
 import DataLoader from './../../src/app/components/DataLoader/DataLoader';
+import Content from './../../src/app/components/Content/Content';
 import dataLoaderUtil from '@dataLoaderUtil';
 
-describe('DataLoader', () => {
+describe.only('DataLoader', () => {
   let dataLoaderUtilSpy;
   const location = { pathname: '', search: '' };
   let wrapper;
+  let component;
   before(() => {
     const children = (<div />);
     dataLoaderUtilSpy = sinon.spy(dataLoaderUtil, 'loadDataForRoutes');
-    wrapper = shallow(<DataLoader location={location} children={children} />);
+    component = (<DataLoader location={location} children={children} />);
+    wrapper = shallow(component);
   });
   after(() => {
     dataLoaderUtilSpy.restore();
   });
+  it('should initialize loaded to null', () => {
+    expect(wrapper.state().loaded).to.equal(null);
+  })
   it('should call dataLoaderUtil with location', () => {
     expect(dataLoaderUtilSpy.calledWith(location)).to.equal(true);
   });
+  it('should call dataLoaderUtil with updateState', () => {
+    expect(dataLoaderUtilSpy.firstCall.args[4].name).to.equal('bound updateState');
+  });
   it('should render the children', () => {
     expect(wrapper.find('div')).to.have.lengthOf(1);
+  });
+  it('should render the content', () => {
+    expect(wrapper.find(Content)).to.have.lengthOf(1);
+  });
+  it('should pass the location prop to content', () => {
+    expect(wrapper.find(Content).props().location).to.equal(wrapper.props().location);
+  });
+  it('should pass the loaded slice of state to content', () => {
+    expect(wrapper.find(Content).props().loaded).to.equal(wrapper.state().loaded);
+  });
+  it('should pass the children to content', () => {
+    expect(wrapper.find(Content).props().children).to.equal(wrapper.props().children);
+  });
+
+  describe('updateState', () => {
+    let setStateSpy;
+    before(() => {
+      setStateSpy = sinon.spy(wrapper.instance(), 'setState');
+    });
+    after(() => {
+      setStateSpy.restore();
+    });
+    it('should call setState with input', () => {
+      wrapper.instance().updateState('fake');
+      expect(setStateSpy.firstCall.args).to.deep.equal([{ loaded: 'fake' }]);
+    });
   });
 });
