dac406b75df516b429d46e2a93119e4acb08e740
Serena Berry <serenaberry@nypl.org>
2020-08-21 13:40:22 -0400

    simplify reducer

--- src/app/stores/Reducers.js
+++ src/app/stores/Reducers.js
@@ -1,150 +1,41 @@
-import { combineReducers } from 'redux';
 import { Actions } from '../actions/Actions';
+import { initialState } from './InitialState';
+import appConfig from '../data/appConfig';
 
-import appConfigData from '../data/appConfig';
-
-const loading = (state = null, action) => {
+function appReducer(state = initialState, action) {
   switch (action.type) {
     case Actions.UPDATE_LOADING_STATUS:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const appConfig = (state = appConfigData, action) => {
-  switch (action.type) {
-    case Actions.APP_CONFIG:
-      return action.appConfig;
-    default:
-      return state;
-  }
-};
-
-const searchResults = (state = null, action) => {
-  switch (action.type) {
+      return { ...state, loading: action.payload };
     case Actions.UPDATE_SEARCH_RESULTS:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const drbbResults = (state = null, action) => {
-  switch (action.type) {
+      return { ...state, searchResults: action.payload };
     case Actions.UPDATE_DRBB_RESULTS:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const bib = (state = null, action) => {
-  switch (action.type) {
+      return { ...state, drbbResults: action.payload };
     case Actions.UPDATE_BIB:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const filters = (state = null, action) => {
-  switch (action.type) {
+      return { ...state, bib: action.payload };
     case Actions.UPDATE_FILTERS:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const selectedFilters = (state = null, action) => {
-  switch (action.type) {
+      return { ...state, filters: action.payload };
     case Actions.UPDATE_SELECTED_FILTERS:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const searchKeywords = (state = null, action) => {
-  switch (action.type) {
+      return { ...state, selectedFilters: action.payload };
     case Actions.UPDATE_SEARCH_KEYWORDS:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const field = (state = null, action) => {
-  switch (action.type) {
+      return { ...state, searchKeywords: action.payload };
     case Actions.UPDATE_FIELD:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const sortBy = (state = null, action) => {
-  switch (action.type) {
+      return { ...state, field: action.payload };
     case Actions.UPDATE_SORT_BY:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const page = (state = null, action) => {
-  switch (action.type) {
+      return { ...state, sortBy: action.payload };
     case Actions.UPDATE_PAGE:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const deliveryLocations = (state = null, action) => {
-  switch (action.type) {
-    case Actions.UPDATE_DELIVERY_LOCATIONS:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const isEddRequestable = (state = null, action) => {
-  switch (action.type) {
+      return { ...state, page: action.payload };
     case Actions.UPDATE_IS_EDD_REQUESTABLE:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const patron = (state = null, action) => {
-  switch (action.type) {
+      return { ...state, isEddRequestable: action.payload };
     case Actions.UPDATE_PATRON_DATA:
-      return action.payload;
+      return { ...state, patron: action.payload };
+    case Actions.UPDATE_DELIVERY_LOCATIONS:
+      return { ...state, deliveryLocation: action.payload };
+    case Actions.SET_APP_CONFIG:
+      return { ...state, appConfig };
     default:
       return state;
   }
-};
-
-const appReducer = combineReducers({
-  searchResults,
-  loading,
-  appConfig,
-  drbbResults,
-  bib,
-  filters,
-  selectedFilters,
-  searchKeywords,
-  patron,
-  deliveryLocations,
-  isEddRequestable,
-  sortBy,
-  page,
-  field,
-});
+}
 
 export const rootReducer = (state, action) => appReducer(state, action);
 
