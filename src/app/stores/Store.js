e7a0d3e68a5e50bcd3cc75c54060c2a772b5bc55
Daniel Appel <dan.s.appel@gmail.com>
2020-08-24 11:11:33 -0400

    Remove unnecessary initialState in Store

--- src/app/stores/Store.js
+++ src/app/stores/Store.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-underscore-dangle */
 import configureStore from './configureStore';
-import initialState from './InitialState';
 
 const preloadedState = global && global.window && global.window.__PRELOADED_STATE__;
 if (preloadedState) {
   delete global.window.__PRELOADED_STATE__;
 }
 
-const store = configureStore(preloadedState || initialState);
+const store = configureStore(preloadedState);
 
 export default store;
