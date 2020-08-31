822361139ad4d84a012166e28d33d054f359398d
Daniel Appel <dan.s.appel@gmail.com>
2020-08-31 17:36:08 -0400

    Add logic to not take any actions in case of redirect

--- src/server/ApiRoutes/User.js
+++ src/server/ApiRoutes/User.js
@@ -6,7 +6,7 @@ function requireUser(req, res) {
     !req.patronTokenResponse.decodedPatron || !req.patronTokenResponse.decodedPatron.sub) {
     // redirect to login
     const fullUrl = encodeURIComponent(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    if (fullUrl.includes('api')) {
+    if (fullUrl.includes('%2Fapi%2F')) {
       res.json({ redirect: true });
     } else {
       res.redirect(`${appConfig.loginUrl}?redirect_uri=${fullUrl}`);
