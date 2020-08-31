78beb43e4c1b66fd859aa3a8c7c871e2c7287102
Daniel Appel <dan.s.appel@gmail.com>
2020-08-31 16:05:01 -0400

    Change if to if else in requireUser

--- src/server/ApiRoutes/User.js
+++ src/server/ApiRoutes/User.js
@@ -6,8 +6,11 @@ function requireUser(req, res) {
     !req.patronTokenResponse.decodedPatron || !req.patronTokenResponse.decodedPatron.sub) {
     // redirect to login
     const fullUrl = encodeURIComponent(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    if (fullUrl.includes('api')) res.json({ redirect: true });
-    res.redirect(`${appConfig.loginUrl}?redirect_uri=${fullUrl}`);
+    if (fullUrl.includes('api')) {
+      res.json({ redirect: true });
+    } else {
+      res.redirect(`${appConfig.loginUrl}?redirect_uri=${fullUrl}`);
+    }
     return false;
   }
   return true;
