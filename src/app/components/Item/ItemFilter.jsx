ba5093ec0c715ee397770a742eebf08bce15d1d5
Serena Berry <serenaberry@nypl.org>
2020-10-06 16:31:57 -0400

    Implement FocusTrap for hiding and revealing filter selections

--- src/app/components/Item/ItemFilter.jsx
+++ src/app/components/Item/ItemFilter.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Checkbox, Button, Icon } from '@nypl/design-system-react-components';
+import FocusTrap from 'focus-trap-react';
 
 import { isOptionSelected } from '../../utils/utils';
 
@@ -9,15 +10,6 @@ const ItemFilter = ({ filter, options, open, manageFilterDisplay }, context) => 
   const { location, createHref } = router;
   const initialFilters = location.query || {};
   const [selectedFilters, updateSelectedFilters] = useState(initialFilters);
-
-  const distinctOptions = Array.from(new Set(options.reduce((optionIds, option) => {
-    if (option.id) optionIds.push(option.id);
-    return optionIds;
-  }, [])))
-    .map(id => ({
-      id,
-      label: options.find(option => option.id === id).label,
-    }));
 
   const selectFilter = (value) => {
     const updatedSelectedFilters = selectedFilters;
@@ -41,61 +33,87 @@ const ItemFilter = ({ filter, options, open, manageFilterDisplay }, context) => 
 
   const handleCheckbox = (option) => {
     const currentSelection = selectedFilters[filter];
-    if (currentSelection && currentSelection.includes(option)) {
+    if (currentSelection && currentSelection.includes(option.id)) {
       deselectFilter(option);
     } else selectFilter(option);
   };
 
   const submitFilterSelections = () => {
-    const href = createHref({ ...location, ...{ query: selectedFilters, hash: '#item-filters', search: '' } });
+    const href = createHref({
+      ...location,
+      ...{
+        query: selectedFilters,
+        hash: '#item-filters',
+        search: '',
+      },
+    });
+    manageFilterDisplay('none');
 
     router.push(href);
   };
 
   const isSelected = (option) => {
-    const { query } = context.router.location;
+    const { query } = location;
     if (!query) return false;
     const result = isOptionSelected(query[filter], option.id);
 
     return result;
   };
 
+  const distinctOptions = Array.from(new Set(options.reduce((optionIds, option) => {
+    if (option.id) optionIds.push(option.id);
+    return optionIds;
+  }, [])))
+    .map(id => ({
+      id,
+      label: options.find(option => option.id === id).label,
+    }));
+
   return (
     <div
       className="item-filter"
-      onBlur={() => manageFilterDisplay('none')}
     >
-      <Button
-        className="item-filter-button"
-        buttonType="outline"
-        onClick={() => manageFilterDisplay(filter)}
-        type="button"
+      <FocusTrap
+        focusTrapOptions={{
+          clickOutsideDeactivates: true,
+          onDeactivate: () => manageFilterDisplay('none'),
+        }}
+        active={open}
       >
-        {filter} <Icon name={open ? 'minus' : 'plus'} />
-      </Button>
-      {open ? (
-        <div className="item-filter-content">
-          <ul>
-            {distinctOptions.map((option, i) => (
-              <li key={option.id || i}>
-                <Checkbox
-                  labelOptions={{
-                    id: option.id,
-                    labelContent: option.label,
-                  }}
-                  onChange={() => handleCheckbox(option)}
-                  key={option.id || i}
-                  isSelected={isSelected(option)}
-                />
-              </li>
-            ))}
-          </ul>
-          <div className="item-filter-buttons">
-            <Button buttonType="link">Clear</Button>
-            <Button onClick={submitFilterSelections}>Save</Button>
+        <Button
+          className="item-filter-button"
+          buttonType="outline"
+          onClick={() => manageFilterDisplay(filter)}
+          type="button"
+        >
+          {filter} <Icon name={open ? 'minus' : 'plus'} />
+        </Button>
+        {open ? (
+          <div
+            className="item-filter-content"
+          >
+            <ul>
+              {distinctOptions.map((option, i) => (
+                <li key={option.id || i}>
+                  <Checkbox
+                    labelOptions={{
+                      id: option.id,
+                      labelContent: option.label,
+                    }}
+                    onChange={() => handleCheckbox(option)}
+                    key={option.id || i}
+                    isSelected={isSelected(option)}
+                  />
+                </li>
+              ))}
+            </ul>
+            <div className="item-filter-buttons">
+              <Button buttonType="link">Clear</Button>
+              <Button onClick={submitFilterSelections}>Save</Button>
+            </div>
           </div>
-        </div>
-      ) : null}
+        ) : null}
+      </FocusTrap>
     </div>
   );
 };
