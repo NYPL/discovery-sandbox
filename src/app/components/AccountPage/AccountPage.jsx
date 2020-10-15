0d4faeca81859dc214627da2ee63d6441b7ee40f
Serena Berry <serenaberry@nypl.org>
2020-10-15 14:31:46 -0400

    Cleanup

--- src/app/components/AccountPage/AccountPage.jsx
+++ src/app/components/AccountPage/AccountPage.jsx
@@ -1,4 +1,4 @@
-/* global window */
+/* global window, document */
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -16,9 +16,10 @@ const AccountPage = (props, { router }) => {
 
     const iframe = document.getElementById('accountContentIframe');
 
-    iframe.onload = function () {
+    iframe.onload = () => {
       // manipulate iframe here
-    }
+      console.log(document.getElementById('accountContentIframe').contentWindow);
+    };
   });
 
   const patronInfoToDisplay = router.location.hash.substring(1) || 'items';
@@ -26,23 +27,22 @@ const AccountPage = (props, { router }) => {
   return (
     <div className="nypl-full-width-wrapper nypl-patron-page">
       <div className="nypl-patron-details">
-          {patron.names[0]}
-          <br />
-          Email: {patron.emails[0]}
+        {patron.names[0]}
+        <br />
+        Email: {patron.emails[0]}
       </div>
       <ul>
-        <li><a href='#items'>Checkouts</a></li>
-        <li><a href='#holds'>Holds</a></li>
-        <li><a href='#mylists'>My Lists</a></li>
-        <li><a href='#overdues'>Fines{`${patron.moneyOwed ? ` ($${patron.moneyOwed.toFixed(2)})` : null}`}</a></li>
-        <li><a href='#msg'>Messages</a></li>
+        <li><a href="#items">Checkouts</a></li>
+        <li><a href="#holds">Holds</a></li>
+        <li><a href="#mylists">My Lists</a></li>
+        <li><a href="#overdues">Fines{`${patron.moneyOwed ? ` ($${patron.moneyOwed.toFixed(2)})` : null}`}</a></li>
+        <li><a href="#msg">Messages</a></li>
       </ul>
       <a
         id="modInfoPopupWindowLinkComponent"
         href={`https://ilsstaff.nypl.org:443/patroninfo*eng~Sdefault/${patron.id}/modpinfo`}
         target="_blank"
-      >
-				My Settings
+      >My Settings
       </a>
       <iframe
         src={`https://ilsstaff.nypl.org:443/dp/patroninfo*eng~Sdefault/${patron.id}/${patronInfoToDisplay}`}
@@ -51,7 +51,7 @@ const AccountPage = (props, { router }) => {
         width="750"
         height="600"
       />
-  </div>
+    </div>
   );
 };
 
