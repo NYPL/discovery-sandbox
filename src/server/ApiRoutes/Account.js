8f4524ae9cb07626eac4affc8671e5e2ad2f2d8e
siberry <siberry226@gmail.com>
2020-10-22 13:20:44 -0400

    Try sending patron cookie with request for HTML

--- src/server/ApiRoutes/Account.js
+++ src/server/ApiRoutes/Account.js
@@ -5,7 +5,14 @@ import { updateAccountHtml } from '../../app/actions/Actions';
 function fetchAccountPage(req, res, next) {
   const { patronId } = req.params;
   const { dispatch } = global.store;
-  axios(`https://ilsstaff.nypl.org:443/dp/patroninfo*eng~Sdefault/${patronId}/items`)
+  const nyplIdentityCookieString = req.cookies.nyplIdentityPatron;
+  const encodedNyplIdentityCookie = Buffer.from(nyplIdentityCookieString).toString("base64");
+
+  axios.get(`https://ilsstaff.nypl.org:443/dp/patroninfo*eng~Sdefault/${patronId}/items`, {
+    headers: {
+      Cookie: `nyplIdentityPatron=${encodedNyplIdentityCookie}`,
+    },
+  })
     .then(resp => {
       dispatch(updateAccountHtml(resp.data));
       next();
