34bbc044b3a56c4d9c954df83c53fd732724dbae
Daniel Appel <dan.s.appel@gmail.com>
2020-12-23 14:08:41 -0500

    Add console log in ItemTable

--- src/app/components/Item/ItemTableRow.jsx
+++ src/app/components/Item/ItemTableRow.jsx
@@ -51,6 +51,7 @@ class ItemTableRow extends React.Component {
     const { closedLocations, recapClosedLocations, nonRecapClosedLocations } = appConfig;
     const isRecap = item.isRecap;
     const allClosed = closedLocations.concat((isRecap ? recapClosedLocations : nonRecapClosedLocations)).includes('');
+    console.log('closedLocations: ', closedLocations, allClosed);
     const status = item.status && item.status.prefLabel ? item.status.prefLabel : ' ';
     let itemRequestBtn = status;
 
