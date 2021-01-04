47d6713747d7fe64b303a7b65d71ca188eeec8cb
Serena Berry <serenaberry@nypl.org>
2021-01-04 11:19:37 -0500

    Email working - includes email, feedback, relative path of page

--- src/server/ApiRoutes/Feedback.js
+++ src/server/ApiRoutes/Feedback.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import aws from 'aws-sdk';
 
 import appConfig from '../../app/data/appConfig';
@@ -12,6 +11,9 @@ export default {
 
     aws.config.update({ region: 'us-east-1' });
 
+    const emailText = Object.entries(fields).map(([field, value]) => `${field}: ${value}`).join(', ');
+    const emailHtml = `<div>${Object.entries(fields).map(([field, value]) => `<p>${field}: ${value}</p>`).join('')}</div>`;
+
     // Create sendEmail params
     const params = {
       Destination: { /* required */
@@ -21,11 +23,11 @@ export default {
         Body: { /* required */
           Html: {
             Charset: 'UTF-8',
-            Data: 'HTML_FORMAT_BODY',
+            Data: emailHtml,
           },
           Text: {
             Charset: 'UTF-8',
-            Data: 'TEXT_FORMAT_BODY',
+            Data: emailText,
           },
         },
         Subject: {
@@ -40,22 +42,8 @@ export default {
     // Create the promise and SES service object
     const sendPromise = new aws.SES({ apiVersion: '2010-12-01' }).sendEmail(params).promise();
 
-    // Handle promise's fulfilled/rejected states
     return sendPromise
       .then(data => console.log(data))
       .catch(err => console.error(err, err.stack));
-    // return axios({
-    //   method: 'POST',
-    //   url: appConfig.feedbackFormUrl,
-    //   data: {
-    //     fields,
-    //   },
-    //   headers: {
-    //     Authorization: `Bearer ${appConfig.airtableApiKey}`,
-    //     'Content-Type': 'application/json',
-    //   },
-    // }).then((postResp) => {
-    //   return res.json(postResp.data);
-    // }).catch(error => res.status(500).json({ error }));
   },
 };
