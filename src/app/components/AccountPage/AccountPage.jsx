fbeb61c4a149bd4d967afeca9aae05ab41b7f4ad
Serena Berry <serenaberry@nypl.org>
2021-01-22 16:10:05 -0500

    Add Hero and Breadcrumb to My Account

--- src/app/components/AccountPage/AccountPage.jsx
+++ src/app/components/AccountPage/AccountPage.jsx
@@ -3,7 +3,16 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { SkeletonLoader, Heading, Button, ButtonTypes } from '@nypl/design-system-react-components';
+import {
+  SkeletonLoader,
+  Heading,
+  Button,
+  ButtonTypes,
+  Breadcrumbs,
+  Hero,
+  HeroTypes,
+  Link,
+} from '@nypl/design-system-react-components';
 
 import LinkTabSet from './LinkTabSet';
 import AccountSettings from './AccountSettings';
@@ -84,74 +93,113 @@ const AccountPage = (props) => {
   };
 
   return (
-    <div className="nypl-full-width-wrapper drbb-integration nypl-patron-page nypl-ds">
-      <Heading level={2} text="My Account" />
-      <div className="nypl-patron-details">
-        <div className="name">{patron.names ? patron.names[0] : null}</div>
-        <div>{patron.barcodes ? patron.barcodes[0] : null}</div>
-        <div>Expiration Date: {patron.expirationDate}</div>
-      </div>
-      {itemToCancel ? (
-        <div className="scc-modal">
-          <div>
-            <p>You requested <span>canceling</span> of following item:</p>
-            <p>{itemToCancel.title}</p>
-            <Button
-              buttonType={ButtonTypes.Secondary}
-              onClick={() => setItemToCancel(null)}
-            >Back
-            </Button>
-            <Button
-              buttonType={ButtonTypes.Primary}
-              onClick={cancelItem}
-            >Confirm
-            </Button>
-          </div>
+    <div className="nypl-ds nypl--research layout-container">
+      <main className="main" id="mainContent">
+        <div className="content-header">
+          <Breadcrumbs
+            breadcrumbs={[
+              { url: "htttps://www.nypl.org", text: "Home" },
+              { url: "https://www.nypl.org/research", text: "Research" },
+              { url: appConfig.baseUrl, text: "Research Catalog" },
+            ]}
+            className="breadcrumbs"
+          />
+          <Hero
+            heroType={HeroTypes.Secondary}
+            heading={(
+              <>
+                <Heading
+                  level={1}
+                  id={"1"}
+                  text="Research Catalog"
+                />
+                <nav
+                  className="sub-nav apply-brand-styles"
+                  aria-label="sub-nav"
+                >
+                  <Link href="#">Search</Link> |&nbsp;
+                  <Link href="#">Subject Heading Explorer</Link> |&nbsp;
+                  <Link href="#">My Account</Link>
+                </nav>
+              </>
+            )}
+            className="apply-brand-styles hero"
+          />
         </div>
-      ) : null}
-      <LinkTabSet
-        activeTab={content}
-        tabs={[
-          {
-            label: 'Checkouts',
-            link: `${baseUrl}/account/items`,
-            content: 'items'
-          },
-          {
-            label: 'Holds',
-            link: `${baseUrl}/account/holds`,
-            content: 'holds',
-          },
-          {
-            label: `Fines${patron.moneyOwed ? ` ($${patron.moneyOwed.toFixed(2)})` : ''}`,
-            link: `${baseUrl}/account/overdues`,
-            content: 'overdues',
-          },
-          {
-            label: 'Account Settings',
-            link: `${baseUrl}/account/settings`,
-            content: 'settings',
-          },
-        ]}
-      />
-      {isLoading && content !== 'settings' ? <SkeletonLoader /> : ''}
-      {
-        typeof accountHtml === 'string' && content !== 'settings' ? (
-          <div
-            dangerouslySetInnerHTML={{ __html: accountHtml }}
-            id="account-page-content"
-            className={`${content} ${isLoading ? 'loading' : ''}`}
+        <div className="content-primary page-content nypl-patron-page">
+          <Heading
+            level={2}
+            id="2"
+            text="My Account"
           />
-        ) : ''
-      }
-      {
-        content === 'settings' ? (
-          <AccountSettings
-            patron={patron}
-            legacyCatalog={appConfig.legacyCatalog}
+          <div className="nypl-patron-details">
+            <div className="name">{patron.names ? patron.names[0] : null}</div>
+            <div>{patron.barcodes ? patron.barcodes[0] : null}</div>
+            <div>Expiration Date: {patron.expirationDate}</div>
+          </div>
+          {itemToCancel ? (
+            <div className="scc-modal">
+              <div>
+                <p>You requested <span>canceling</span> of following item:</p>
+                <p>{itemToCancel.title}</p>
+                <Button
+                  buttonType={ButtonTypes.Secondary}
+                  onClick={() => setItemToCancel(null)}
+                >Back
+                </Button>
+                <Button
+                  buttonType={ButtonTypes.Primary}
+                  onClick={cancelItem}
+                >Confirm
+                </Button>
+              </div>
+            </div>
+          ) : null}
+          <LinkTabSet
+            activeTab={content}
+            tabs={[
+              {
+                label: 'Checkouts',
+                link: `${baseUrl}/account/items`,
+                content: 'items'
+              },
+              {
+                label: 'Holds',
+                link: `${baseUrl}/account/holds`,
+                content: 'holds',
+              },
+              {
+                label: `Fines${patron.moneyOwed ? ` ($${patron.moneyOwed.toFixed(2)})` : ''}`,
+                link: `${baseUrl}/account/overdues`,
+                content: 'overdues',
+              },
+              {
+                label: 'Account Settings',
+                link: `${baseUrl}/account/settings`,
+                content: 'settings',
+              },
+            ]}
           />
-        ) : null
-      }
+          {isLoading && content !== 'settings' ? <SkeletonLoader /> : ''}
+          {
+            typeof accountHtml === 'string' && content !== 'settings' ? (
+              <div
+                dangerouslySetInnerHTML={{ __html: accountHtml }}
+                id="account-page-content"
+                className={`${content} ${isLoading ? 'loading' : ''}`}
+              />
+            ) : ''
+          }
+          {
+            content === 'settings' ? (
+              <AccountSettings
+                patron={patron}
+                legacyCatalog={appConfig.legacyCatalog}
+              />
+            ) : null
+          }
+        </div>
+      </main>
     </div>
   );
 };
