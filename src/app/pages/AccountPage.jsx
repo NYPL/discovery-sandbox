c1904eb46e31db1c3a430ed7e03d7454df474d18
Daniel Appel <dan.s.appel@gmail.com>
2021-03-12 16:25:56 -0500

    Fix incorrect merge in AccountPage

--- src/app/pages/AccountPage.jsx
+++ src/app/pages/AccountPage.jsx
@@ -18,6 +18,7 @@ import LinkTabSet from '../components/AccountPage/LinkTabSet';
 import AccountSettings from '../components/AccountPage/AccountSettings';
 import LoadingLayer from '../components/LoadingLayer/LoadingLayer';
 import AccountPageModal from '../components/AccountPageModal/AccountPageModal';
+import SccContainer from '../components/SccContainer/SccContainer';
 import { logOutFromEncoreAndCatalogIn } from '../utils/logoutUtils';
 
 import { manipulateAccountPage, makeRequest, buildReqBody } from '../utils/accountPageUtils';
@@ -122,136 +123,91 @@ const AccountPage = (props) => {
   }
 
   return (
-    <div className="nypl-ds nypl--research layout-container">
+    <SccContainer
+      className="page-content nypl-patron-page"
+      activeSection="account"
+      pageTitle="Account"
+    >
+      <Heading
+        level={2}
+        id="2"
+        text="My Account"
+      />
       {
         accountPageModal ?
-        <AccountPageModal
-          stayLoggedIn={resetCountdown}
-          baseUrl={baseUrl}
-        /> :
-        null
+          <AccountPageModal
+            stayLoggedIn={resetCountdown}
+            baseUrl={baseUrl}
+          /> :
+          null
       }
-      <main className="main" id="mainContent">
-        <div className="content-header">
-          <Breadcrumbs
-            breadcrumbs={[
-              { url: "htttps://www.nypl.org", text: "Home" },
-              { url: "https://www.nypl.org/research", text: "Research" },
-              { url: baseUrl, text: "Research Catalog" },
-            ]}
-            className="breadcrumbs"
-          />
-          <Hero
-            heroType={HeroTypes.Secondary}
-            heading={(
-              <>
-                <Heading
-                  level={1}
-                  id={"1"}
-                  text="Research Catalog"
-                />
-                <nav
-                  className="sub-nav apply-brand-styles"
-                  aria-label="sub-nav"
-                >
-                  <Link
-                    className="sub-nav__link"
-                    href={baseUrl}
-                  >
-                    Search
-                  </Link> |&nbsp;
-                  <Link
-                    className="sub-nav__link"
-                    href={`${baseUrl}/subject_headings`}
-                  >
-                    Subject Heading Explorer
-                  </Link> |&nbsp;
-                  <span
-                    className="sub-nav__active-section"
-                  >
-                    My Account
-                  </span>
-                </nav>
-              </>
-            )}
-            className="apply-brand-styles hero"
-          />
-        </div>
-        <div className="content-primary page-content nypl-patron-page">
-          <Heading
-            level={2}
-            id="2"
-            text="My Account"
-          />
-          <div className="nypl-patron-details">
-            <div className="name">{patron.names ? patron.names[0] : null}</div>
-            <div>{patron.barcodes ? patron.barcodes[0] : null}</div>
-            <div>Expiration Date: {formattedExpirationDate}</div>
+      <div className="nypl-patron-details">
+        <div className="name">{patron.names ? patron.names[0] : null}</div>
+        <div>{patron.barcodes ? patron.barcodes[0] : null}</div>
+        <div>Expiration Date: {formattedExpirationDate}</div>
+      </div>
+      {itemToCancel ? (
+        <div className="scc-modal">
+          <div>
+            <p>Cancel your hold on this item?</p>
+            <p>{itemToCancel.title}</p>
+            <Button
+              buttonType={ButtonTypes.Secondary}
+              onClick={() => setItemToCancel(null)}
+            >Back
+            </Button>
+            <Button
+              buttonType={ButtonTypes.Primary}
+              onClick={cancelItem}
+            >Confirm
+            </Button>
           </div>
-          {itemToCancel ? (
-            <div className="scc-modal">
-              <div>
-                <p>Cancel your hold on this item?</p>
-                <p>{itemToCancel.title}</p>
-                <Button
-                  buttonType={ButtonTypes.Secondary}
-                  onClick={() => setItemToCancel(null)}
-                >Back
-                </Button>
-                <Button
-                  buttonType={ButtonTypes.Primary}
-                  onClick={cancelItem}
-                >Confirm
-                </Button>
-              </div>
-            </div>
-          ) : null}
-          <LinkTabSet
-            activeTab={content}
-            tabs={[
-              {
-                label: 'Checkouts',
-                link: `${baseUrl}/account/items`,
-                content: 'items'
-              },
-              {
-                label: 'Holds',
-                link: `${baseUrl}/account/holds`,
-                content: 'holds',
-              },
-              {
-                label: `Fines${patron.moneyOwed ? ` ($${patron.moneyOwed.toFixed(2)})` : ''}`,
-                link: `${baseUrl}/account/overdues`,
-                content: 'overdues',
-              },
-              {
-                label: 'Account Settings',
-                link: `${baseUrl}/account/settings`,
-                content: 'settings',
-              },
-            ]}
-          />
-          {isLoading && content !== 'settings' ? <SkeletonLoader /> : ''}
-          {
-            typeof accountHtml === 'string' && content !== 'settings' ? (
-              <div
-                dangerouslySetInnerHTML={{ __html: accountHtml }}
-                id="account-page-content"
-                className={`${content} ${isLoading ? 'loading' : ''}`}
-              />
-            ) : ''
-          }
-          {
-            content === 'settings' ? (
-              <AccountSettings
-                patron={patron}
-                legacyCatalog={appConfig.legacyCatalog}
-              />
-            ) : null
-          }
         </div>
-      </main>
-    </div>
+      ) : null}
+      <LinkTabSet
+        activeTab={content}
+        tabs={[
+          {
+            label: 'Checkouts',
+            link: `${baseUrl}/account/items`,
+            content: 'items'
+          },
+          {
+            label: 'Holds',
+            link: `${baseUrl}/account/holds`,
+            content: 'holds',
+          },
+          {
+            label: `Fines${patron.moneyOwed ? ` ($${patron.moneyOwed.toFixed(2)})` : ''}`,
+            link: `${baseUrl}/account/overdues`,
+            content: 'overdues',
+          },
+          {
+            label: 'Account Settings',
+            link: `${baseUrl}/account/settings`,
+            content: 'settings',
+          },
+        ]}
+      />
+      {isLoading && content !== 'settings' ? <SkeletonLoader /> : ''}
+      {
+        typeof accountHtml === 'string' && content !== 'settings' ? (
+          <div
+            dangerouslySetInnerHTML={{ __html: accountHtml }}
+            id="account-page-content"
+            className={`${content} ${isLoading ? 'loading' : ''}`}
+          />
+        ) : ''
+      }
+      {
+        content === 'settings' ? (
+          <AccountSettings
+            patron={patron}
+            legacyCatalog={appConfig.legacyCatalog}
+          />
+        ) : null
+      }
+    </SccContainer>
   );
 };
 
