2ecccfaaa1ee6fe96df3b9c1b08544ea8185a9b4
Serena Berry <serenaberry@nypl.org>
2021-03-16 11:10:45 -0400

    Basic tests for `ItemFilter`, including "Clear" functionality

---
+++ test/unit/ItemFilter.test.js
@@ -0,0 +1,64 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-env mocha */
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import { spy } from 'sinon';
+
+import ItemFilter from './../../src/app/components/Item/ItemFilter';
+import { locationFilters, statusFilters } from '../fixtures/itemFilterOptions';
+
+describe('ItemFilters', () => {
+  describe('default rendering', () => {
+    let component;
+    it('should not render without an `items` prop', () => {
+      component = shallow(<ItemFilter />);
+      expect(component.type()).to.equal(null);
+    });
+  });
+
+  describe('with `options`', () => {
+    let component;
+    it('should render a `div` and a `button`', () => {
+      component = mount(<ItemFilter options={locationFilters} />);
+      expect(component.find('div').length).to.equal(1);
+      expect(component.find('button').length).to.equal(1);
+    });
+  });
+
+  describe('with `options` open state', () => {
+    let component;
+    it('should render a fieldset, 3 buttons', () => {
+      component = mount(
+        <ItemFilter
+          options={locationFilters}
+          isOpen
+        />);
+      expect(component.find('fieldset').length).to.equal(1);
+      expect(component.find('button').length).to.equal(3);
+    });
+  });
+
+  describe('with `selectedFilters`', () => {
+    it('clear button should remove selected filters for corresponding `filter`', () => {
+      let filterSubmission;
+      const component = mount(
+        <ItemFilter
+          options={locationFilters}
+          isOpen
+          selectedFilters={{
+            location: ['loc:maj03', 'offsite'],
+            status: ['status:a'],
+          }}
+          filter="location"
+          submitFilterSelections={(input) => {
+            filterSubmission = input;
+          }}
+        />);
+      const clearButton = component.find('button').at(1);
+      expect(clearButton.text()).to.equal('Clear');
+      clearButton.simulate('click');
+      expect(filterSubmission).to.deep.equal({ location: [], status: ['status:a'] });
+    });
+  });
+});
