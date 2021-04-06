ff0663d1a93404c2b5061cdd1c7779d5f4f9c97a
Daniel Appel <dan.s.appel@gmail.com>
2021-04-06 12:12:08 -0400

    Remove testing url in logoututil

--- src/app/utils/logoutUtils.js
+++ src/app/utils/logoutUtils.js
@@ -22,8 +22,7 @@ const loadLogoutIframe = (onload) => {
 
   logoutIframe.setAttribute(
     // The endpoint is the URL for logging out from Encore
-    // 'src', 'https://browse.nypl.org/iii/encore/logoutFilterRedirect?suite=def',
-    'src', 'https://browse.nypl.org',
+    'src', 'https://browse.nypl.org/iii/encore/logoutFilterRedirect?suite=def',
     // 'src', 'https://nypl-encore-test.nypl.org//iii/encore/logoutFilterRedirect?suite=def',
   );
   // Assigns the ID for CSS ussage
