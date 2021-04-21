a8b289b798bd02b1639f41ab62f40ca1f2c1bc5e
Daniel Appel <dan.s.appel@gmail.com>
2021-04-21 14:04:51 -0400

    Add api response handling tests for Feedback form

--- test/unit/Feedback.test.js
+++ test/unit/Feedback.test.js
@@ -9,7 +9,7 @@ import appConfig from '../../src/app/data/appConfig';
 
 import Feedback from './../../src/app/components/Feedback/Feedback';
 
-describe.only('Feedback', () => {
+describe('Feedback', () => {
   let component;
   const onSubmitFormSpy = spy(Feedback.prototype, 'onSubmitForm');
   before(() => {
@@ -61,18 +61,11 @@ describe.only('Feedback', () => {
     expect(submitButton.props().type).to.equal('submit');
   });
 
-  // it('should call `onSubmitForm` when form is submitted', () => {
-  //   const submitButton = component.find('Button').at(2).find('button');
-  //   submitButton.simulate('submit');
-  //   expect(onSubmitFormSpy.calledOnce).to.equal(true);
-  // });
-
   describe('entering text', () => {
     it('should record text typed into form', () => {
       const textarea = component.find('textarea');
       textarea.instance().value = 'Test text';
       textarea.simulate('change');
-      console.log('state: ', component.state(), textarea.text());
       expect(textarea.text()).to.equal('Test text');
     });
   });
@@ -81,10 +74,8 @@ describe.only('Feedback', () => {
 
     let savedBaseUrl;
     let savedSetState;
-    before(() => {
+    let apiCalled;
 
-      // component.update();
-    });
     after(() => {
       appConfig.baseUrl = savedBaseUrl;
     });
@@ -92,11 +83,77 @@ describe.only('Feedback', () => {
     it('should submit form when submit is pressed', () => {
       savedBaseUrl = appConfig.baseUrl;
       appConfig.baseUrl = 'http://test-server.com';
-      console.log('it clause');
+
       return new Promise((resolve) => {
         savedSetState = component.instance().setState.bind(component.instance());
         component.instance().setState = (...args) => {
           savedSetState(...args, () => { resolve(); });
+        };
+
+        nock('http://test-server.com')
+          .defaultReplyHeaders({
+            'access-control-allow-origin': '*',
+            'access-control-allow-credentials': 'true',
+          })
+          .post(/\/api\/feedback/)
+          .reply(200, () => {
+            apiCalled = true;
+            return {};
+          });
+
+        const submitButton = component.find('Button').at(2).find('button');
+        submitButton.simulate('submit');
+      }).then(() => {
+        expect(apiCalled).to.equal(true);
+      });
+    });
+
+    it('should not show We are here to help message or form', () => {
+      component.update();
+      const form = component.find('form');
+      const message = component.html();
+      expect(form.length).to.equal(0);
+      expect(message).not.to.include('We are here to help');
+    });
+
+    it('should show thank you message', () => {
+      const ptag = component.find('p');
+      const expectedText = 'Thank you for submitting your comments. ' +
+      'If you requested a response, our service staff ' +
+      'will get back to you as soon as possible.';
+      expect(ptag.text()).to.equal(expectedText);
+    });
+  });
+
+  describe('handling form failure in case of response with errors', () => {
+    component = mount(<Feedback />);
+    const textarea = component.find('textarea');
+    textarea.instance().value = 'Test text';
+    textarea.simulate('change');
+    const submitButton = component.find('Button').at(2);
+    const originalError = console.error;
+    let erroredCorrectly = false;
+
+
+    let savedBaseUrl;
+
+    after(() => {
+      appConfig.baseUrl = savedBaseUrl;
+      console.error = originalError;
+    });
+
+    it('should log the error', () => {
+      savedBaseUrl = appConfig.baseUrl;
+      appConfig.baseUrl = 'http://test-server.com';
+
+      return new Promise((resolve) => {
+
+        console.error = (...args) => {
+          originalError(...args);
+          if (args.includes('errorText')) {
+            erroredCorrectly = true;
+            resolve();
+          }
         };
         nock('http://test-server.com')
           .defaultReplyHeaders({
@@ -105,22 +162,59 @@ describe.only('Feedback', () => {
           })
           .post(/\/api\/feedback/)
           .reply(200, () => {
-            console.log('nock intercepted');
-            // setImmediate(() => { resolve(); });
-            // setTimeout(() => { resolve(); }, 200);
-            return {};
+            return { error: 'errorText' };
           });
-        // nock.emitter.on('replied', () => {
-        //   console.log('replied');
-        //   return setImmediate(() => resolve());
-        // });
-        console.log('component start state: ', component.state());
-        const submitButton = component.find('Button').at(2).find('button');
+
         submitButton.simulate('submit');
-        // setImmediate(() => resolve());
       }).then(() => {
-        console.log('component new state: ', component.state());
-        // done();
+        expect(erroredCorrectly).to.equal(true);
+      });
+    });
+  });
+
+  describe('handling form failure in case of response with error status', () => {
+    component = mount(<Feedback />);
+    const textarea = component.find('textarea');
+    textarea.instance().value = 'Test text';
+    textarea.simulate('change');
+    const submitButton = component.find('Button').at(2);
+    const originalLog = console.log;
+    let loggedCorrectly = false;
+
+
+    let savedBaseUrl;
+
+    after(() => {
+      appConfig.baseUrl = savedBaseUrl;
+      console.error = originalLog;
+    });
+
+    it('should log the error', () => {
+      savedBaseUrl = appConfig.baseUrl;
+      appConfig.baseUrl = 'http://test-server.com';
+
+      return new Promise((resolve) => {
+
+        console.log = (...args) => {
+          originalLog(...args);
+          if (args.includes('Feedback error')) {
+            loggedCorrectly = true;
+            resolve();
+          }
+        };
+        nock('http://test-server.com')
+          .defaultReplyHeaders({
+            'access-control-allow-origin': '*',
+            'access-control-allow-credentials': 'true',
+          })
+          .post(/\/api\/feedback/)
+          .reply(500, () => {
+            return { error: 'errorText' };
+          });
+
+        submitButton.simulate('submit');
+      }).then(() => {
+        expect(loggedCorrectly).to.equal(true);
       });
     });
   });
