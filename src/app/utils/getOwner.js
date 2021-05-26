87e3f4771dea0211f106a0804851eab831d5b362
Paul Beaudoin <contact@paulbeaudoin.com>
2021-05-26 16:28:18 -0400

    Change utils/getOwner to identify Harvard items
    Changes utils/getOwner to identify partner items generally and derive a
    suitable institution label. Adds tests.

--- src/app/utils/getOwner.js
+++ src/app/utils/getOwner.js
@@ -4,38 +4,20 @@ import {
 /*
 * getOwner(bib)
 * This is currently only for non-NYPL partner items. If it's NYPL, it should return undefined.
-* Requirement: Look at all the owners of all the items and if they were all the same and
-* not NYPL, show that as the owning institution and otherwise show nothing.
-* @param {object} bibId
+* @param {object} bib
 * @return {string}
 */
 
 
 export default function getOwner(bib) {
-  if (!bib) return null;
+  if (!bib || !bib.items) return null;
 
-  const items = bib.items;
-  const ownerArr = [];
-  let owner;
-
-  if (!items || !items.length) {
-    return null;
-  }
-
-  items.forEach((item) => {
-    const ownerObj = item.owner && item.owner.length ? item.owner[0].prefLabel : undefined;
-
-    ownerArr.push(ownerObj);
-  });
-
-  // From above, check to see if all the owners are the same, and if so, proceed if the owner
-  // is either Princeton or Columbia.
-  if (_every(ownerArr, o => (o === ownerArr[0]))) {
-    if ((ownerArr[0] === 'Princeton University Library') ||
-    (ownerArr[0] === 'Columbia University Libraries')) {
-      owner = ownerArr[0];
-    }
-  }
-
-  return owner;
+  return bib.items
+    // Only consider items with a Recap source id
+    .filter((item) => item.idNyplSourceId && item.idNyplSourceId['@type'] && /^Recap/.test(item.idNyplSourceId['@type']))
+    // Only consider items with an `owner` (should be all, in practice)
+    .filter((item) => item.owner && item.owner.length && item.owner[0] && item.owner[0].prefLabel)
+    // Map to owner `prefLabel`
+    .map((item) => item.owner[0].prefLabel)
+    [0];
 }
