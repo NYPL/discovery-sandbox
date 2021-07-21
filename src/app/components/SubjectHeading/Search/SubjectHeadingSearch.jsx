9b47a51f18171690bd8802837b5bbee5a15caae7
danamansana <danielappel@nypl.org>
2021-07-21 17:38:09 -0400

    Add style element for subject heading search

--- src/app/components/SubjectHeading/Search/SubjectHeadingSearch.jsx
+++ src/app/components/SubjectHeading/Search/SubjectHeadingSearch.jsx
@@ -36,7 +36,9 @@ class SubjectHeadingSearch extends React.Component {
         return axios(`${appConfig.baseUrl}/api/subjectHeadings/autosuggest?query=${this.state.userInput}`)
           .then((res) => {
             const numberOfResults = res.data.autosuggest.length;
-            document.getElementById('subject-heading-content-primary').style['min-height'] = `${(numberOfResults > 12 ? numberOfResults : 12) * 50}px`
+            document
+              .getElementById('subject-heading-content-primary-style')
+              .innerHTML = `.content-primary.subject-heading-page#subject-heading-content-primary { min-height: ${(numberOfResults > 12 ? numberOfResults : 12) * 50}px}`;
             if (res.data.request.query.trim() === this.state.userInput.trim()) {
               this.setState({
                 suggestions: res.data.autosuggest,
