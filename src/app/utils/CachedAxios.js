fdd7c5e566a379f265325d6ebacc7292810fbe5c
danamansana <danielappel@nypl.org>
2021-08-12 11:06:02 -0400

    Remove unnecessary new

--- src/app/utils/CachedAxios.js
+++ src/app/utils/CachedAxios.js
@@ -4,9 +4,7 @@ function CachedAxios() {
   this.cache = {};
   this.call = (url) => {
     if (this.cache[url]) {
-      return new Promise((resolve) => {
-        resolve(this.cache[url]);
-      });
+      return Promise.resolve(this.cache[url]);
     }
 
     return axios(url)
