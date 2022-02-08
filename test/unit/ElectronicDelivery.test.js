adabd719043cf24eb69f3fe1b83a03a4a35eff44
wormjim <gcooled29@gmail.com>
2022-02-08 02:16:18 -0500

    Install Storage Mock && Pretty Up EDD Test File

--- test/unit/ElectronicDelivery.test.js
+++ test/unit/ElectronicDelivery.test.js
@@ -1,46 +1,57 @@
 /* eslint-env mocha */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import React from 'react';
 import { mock } from 'sinon';
-import { mountTestRender, makeTestStore } from '../helpers/store';
-
+import { makeTestStore, mountTestRender } from '../helpers/store';
 import ElectronicDeliveryForm from './../../src/app/components/ElectronicDeliveryForm/ElectronicDeliveryForm';
-import ElectronicDelivery from './../../src/app/pages/ElectronicDelivery';
 import appConfig from './../../src/app/data/appConfig';
+import ElectronicDelivery from './../../src/app/pages/ElectronicDelivery';
 
 describe('ElectronicDeliveryForm', () => {
   describe('default form', () => {
     let component;
     let appConfigMock;
+
     before(() => {
       appConfigMock = mock(appConfig);
       appConfig.features = [];
       appConfig.eddAboutUrl.default = 'example.com/edd-default-url';
-      component = shallow(
-        <ElectronicDeliveryForm fromUrl="example.com" />,
-      );
+      component = shallow(<ElectronicDeliveryForm fromUrl='example.com' />);
     });
+
     after(() => {
       appConfigMock.restore();
     });
+
     it('should have `pickupLocation` set to `edd`', () => {
-      expect(component.find('input').findWhere(n => n.props().name === 'pickupLocation').length).to.equal(1);
+      expect(
+        component
+          .find('input')
+          .findWhere((n) => n.props().name === 'pickupLocation').length,
+      ).to.equal(1);
     });
+
     it('should have default EDD about URL', () => {
-      expect(component.find('a').first().prop('href')).to.equal('example.com/edd-default-url');
+      expect(component.find('a').first().prop('href')).to.equal(
+        'example.com/edd-default-url',
+      );
     });
   });
+
   describe('with "on-site-edd" feature flag', () => {
     let component;
     let appConfigMock;
+
     before(() => {
       appConfigMock = mock(appConfig);
       appConfigMock.object.eddAboutUrl = {
         onSiteEdd: 'example.com/scan-and-deliver',
       };
+
       const store = makeTestStore({ features: ['on-site-edd'] });
+
       component = mountTestRender(
         <ElectronicDelivery
           params={{ bibId: 'book1' }}
@@ -51,12 +62,16 @@ describe('ElectronicDeliveryForm', () => {
         { store },
       );
     });
+
     after(() => {
       component.unmount();
     });
+
     it('should have "Scan & Deliver" EDD about URL', () => {
       const form = component.find('ElectronicDeliveryForm');
-      expect(form.find('a').first().prop('href')).to.equal('example.com/scan-and-deliver');
+      expect(form.find('a').first().prop('href')).to.equal(
+        'example.com/scan-and-deliver',
+      );
     });
   });
 });
