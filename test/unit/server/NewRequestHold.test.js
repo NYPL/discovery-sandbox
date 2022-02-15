97a7ddf1085b915ace4beda5b6b82784f3623f3b
wormjim <gcooled29@gmail.com>
2022-02-15 16:11:53 -0500

    Build Hold Request Test Suite With New Hold Request Test

---
+++ test/unit/server/NewRequestHold.test.js
@@ -0,0 +1,45 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import request from 'supertest';
+import LibraryItem from '../../../src/app/utils/item';
+import Bib from '../../../src/server/ApiRoutes/Bib';
+import bibs from '../../fixtures/bibs';
+import app from './../../../server';
+
+describe('Hold Request', () => {
+  const sandbox = sinon.createSandbox();
+  const bibId = 'b21147020';
+  const itemId = 'i34755671';
+
+  describe('New Hold Request', () => {
+    before(function () {
+      sandbox.stub(Bib, 'fetchBib').callsFake((__, cb) => {
+        return new Promise(() => {
+          cb({ bib: bibs[3] });
+        });
+      });
+
+      sandbox.stub(LibraryItem, 'getItem').callsFake(() => {
+        return { barcode: 'abc123' };
+      });
+    });
+
+    after(function () {
+      sandbox.restore();
+    });
+
+    it('Redirects to Aeon if request for special collections', function (done) {
+      request(app)
+        .get(`${process.env.BASE_URL}/hold/request/${bibId}-${itemId}`)
+        .expect(302)
+        .then((res) => {
+          expect(res.text, 'Not redirecting').to.include('Redirecting');
+          expect(res.text, 'None aeon url').to.include('aeon');
+          expect(res.text, 'Unmatched record').to.include(`record=${bibId}`);
+
+          done();
+        })
+        .catch((error) => done(error));
+    });
+  });
+});
