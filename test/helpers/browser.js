78e528ee57b455686091757bd2fcce3f8609b768
Grahn Cooledge <gcooled29@gmail.com>
2022-02-16 12:07:43 -0500

    User EDD Form Session Storage  (#1847)
    FEATEURE: Store Users Form Inputs for Electronic Document Delivery for storage across local storage

--- test/helpers/browser.js
+++ test/helpers/browser.js
@@ -3,6 +3,10 @@
 // Perhaps we can instead point mocah to run a setupfile
 // which then executes this file to set up the browser
 require('dotenv').config({ path: 'test.env' });
+
+require('mock-local-storage');
+global.window = {};
+
 const enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
@@ -17,6 +21,7 @@ const exposedProperties = ['window', 'navigator', 'document'];
 global.document = jsdom('');
 const { document } = global;
 global.window = document.defaultView;
+
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
     exposedProperties.push(property);
@@ -27,6 +32,9 @@ Object.keys(document.defaultView).forEach((property) => {
 global.navigator = {
   userAgent: 'node.js',
 };
+
+window.sessionStorage = global.sessionStorage;
+window.localStorage = global.localStorage;
 
 const noop = () => {};
 require.extensions['.png'] = noop;
