0597aca5cd86b53032d4afb4e4dd4e22131f85f0
wormjim <gcooled29@gmail.com>
2022-03-10 16:49:00 -0500

    Update Item Tests To Ensure Passed Results

--- test/unit/ItemTableRow.test.js
+++ test/unit/ItemTableRow.test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { mount, shallow } from 'enzyme';
 import React from 'react';
-import { spy } from 'sinon';
+import { stub } from 'sinon';
 import appConfig from '../../src/app/data/appConfig';
 import { noop } from '../../src/app/utils/utils';
 import item from '../fixtures/libraryItems';
@@ -31,7 +31,7 @@ describe('ItemTableRow', () => {
     });
   });
 
-  describe('Rendered row', () => {
+  describe('Rendered row[s]', () => {
     describe('Missing data item', () => {
       const data = item.missingData;
       let component;
@@ -40,13 +40,25 @@ describe('ItemTableRow', () => {
         component = shallow(<ItemTableRow item={data} />);
       });
 
+      // This will return a fragment with tr children
+      // On BibPage only one row, others, two rows
       it('should return a <tr>', () => {
-        expect(component.type()).to.equal('tr');
-        expect(component.prop('className')).to.equal('available');
+        const bibPage = shallow(<ItemTableRow item={data} page='BibPage' />);
+        expect(
+          bibPage.find('tr'),
+          'BibPage contains more than one row',
+        ).to.have.lengthOf(1);
+
+        expect(component.find('tr')).to.have.lengthOf(2);
+        expect(component.find('tr').at(0).prop('className')).to.equal(
+          'available',
+        );
       });
 
-      it('should return five <td>', () => {
-        expect(component.find('td').length).to.equal(5);
+      // On Bibpage one row with 4 columns
+      // On other pages, two rows, first row 4 columns, second row one column
+      it('should return four <td>', () => {
+        expect(component.find('td').length).to.equal(4);
       });
 
       it('should not have a format as the first <td> column data', () => {
@@ -62,11 +74,11 @@ describe('ItemTableRow', () => {
       });
 
       it('should not have a call number as the fourth <td> column data', () => {
-        expect(component.find('td').at(3).text()).to.equal(' ');
+        expect(component.find('td').at(1).text()).to.equal(' ');
       });
 
       it('should not have a location as the fifth <td> column data', () => {
-        expect(component.find('td').at(4).text()).to.equal(' ');
+        expect(component.find('td').at(1).text()).to.equal(' ');
       });
     });
 
@@ -75,36 +87,48 @@ describe('ItemTableRow', () => {
       let component;
 
       before(() => {
-        component = shallow(<ItemTableRow item={data} />);
+        component = mount(<ItemTableRow item={data} />);
       });
 
       it('should return a <tr>', () => {
-        expect(component.type()).to.equal('tr');
-        expect(component.prop('className')).to.equal('available');
+        const bibPage = shallow(<ItemTableRow item={data} page='BibPage' />);
+        expect(bibPage.find('tr')).to.have.lengthOf(1);
+
+        expect(component.find('tr')).to.have.lengthOf(2);
+        expect(component.find('tr').at(0).prop('className')).to.equal(
+          'available',
+        );
       });
 
-      it('should return five <td>', () => {
-        expect(component.find('td').length).to.equal(5);
+      it('should return four <td>', () => {
+        expect(component.find('td').length).to.equal(4);
       });
 
+      // Pass
       it('should have a format as the first <td> column data', () => {
         expect(component.find('td').at(0).text()).to.equal('Text');
       });
 
-      it('should have an access message as the second <td> column data', () => {
-        expect(component.find('td').at(1).text()).to.equal('USE IN LIBRARY');
-      });
+      // NOTE: Relevance, are we using this message?
+      // it('should have an access message as the second <td> column data', () => {
+      //   expect(component.find('td').at(1).text()).to.equal('USE IN LIBRARY');
+      // });
 
+      // NOTE: Relevance, should we check for buttons instead?
       it('should have a status as the third <td> column data', () => {
-        expect(component.find('td').at(2).text()).to.equal('Available');
+        const recapWrapper = component.find('ReCAPButton');
+        expect(recapWrapper.text()).to.equal('Available');
+        expect(component.find('td').at(3).render().text()).to.equal(
+          'Available',
+        );
       });
 
-      it('should have a call number as the fourth <td> column data', () => {
-        expect(component.find('td').at(3).text()).to.equal('JFE 07-5007 ---');
+      it('should have a call number as the second <td> column data', () => {
+        expect(component.find('td').at(1).text()).to.equal('JFE 07-5007 ---');
       });
 
-      it('should have a location as the fifth <td> column data', () => {
-        expect(component.find('td').at(4).text()).to.equal(
+      it('should have a location as the third <td> column data', () => {
+        expect(component.find('td').at(2).text()).to.equal(
           'SASB M1 - General Research - Room 315',
         );
       });
@@ -115,24 +139,27 @@ describe('ItemTableRow', () => {
       let component;
 
       before(() => {
-        component = shallow(<ItemTableRow item={data} />);
+        component = mount(<ItemTableRow item={data} />);
       });
 
-      it('should have an access message as the second <td> column data', () => {
-        expect(component.find('td').at(1).text()).to.equal('USE IN LIBRARY');
+      // NOTE: Relevance? Are we using messages
+      // it('should have an access message as the second <td> column data', () => {
+      //   expect(component.find('td').at(1).text()).to.equal('USE IN LIBRARY');
+      // });
+
+      it('should be physically requestable with a Link button', () => {
+        expect(component.find('td').at(3).find('Link').text()).to.equal(
+          'Request for Onsite Use',
+        );
+        expect(component.find('td').at(3).find('Link').length).to.equal(1);
       });
 
-      it('should have a status as the third <td> column data and not a button', () => {
-        expect(component.find('td').at(2).render().text()).to.equal('Request');
-        expect(component.find('td').find('Link').length).to.equal(1);
+      it('should have a call number as the second <td> column data', () => {
+        expect(component.find('td').at(1).text()).to.equal('JFE 07-5007 ---');
       });
 
-      it('should have a call number as the fourth <td> column data', () => {
-        expect(component.find('td').at(3).text()).to.equal('JFE 07-5007 ---');
-      });
-
-      it('should have a location as the fifth <td> column data', () => {
-        expect(component.find('td').at(4).text()).to.equal(
+      it('should have a location as the third <td> column data', () => {
+        expect(component.find('td').at(2).text()).to.equal(
           'SASB M1 - General Research - Room 315',
         );
       });
@@ -143,26 +170,27 @@ describe('ItemTableRow', () => {
       let component;
 
       before(() => {
-        component = shallow(<ItemTableRow item={data} />);
+        component = mount(<ItemTableRow item={data} />);
       });
 
-      it('should have an access message as the second <td> column data', () => {
-        expect(component.find('td').at(1).text()).to.equal('USE IN LIBRARY');
-      });
+      // TODO: NOTE: Relevance? Are we using messages
+      // it('should have an access message as the second <td> column data', () => {
+      //   expect(component.find('td').at(1).text()).to.equal('USE IN LIBRARY');
+      // });
 
       it('should have a call number as the fourth <td> column data', () => {
-        expect(component.find('td').at(3).text()).to.equal('JFE 07-5007 ---');
+        expect(component.find('td').at(1).text()).to.equal('JFE 07-5007 ---');
       });
 
       it('should have a status as the third <td> column data and not a button', () => {
-        expect(component.find('td').at(2).text()).to.equal('Available');
+        expect(component.find('td').at(3).text()).to.equal('Available');
         expect(
-          component.find('td').at(2).render().find('Link').length,
+          component.find('td').at(3).render().find('Link').length,
         ).to.equal(0);
       });
 
       it('should have a location as the fifth <td> column data', () => {
-        expect(component.find('td').at(4).text()).to.equal(
+        expect(component.find('td').at(2).text()).to.equal(
           'SASB M1 - General Research - Room 315',
         );
       });
@@ -173,21 +201,29 @@ describe('ItemTableRow', () => {
       let component;
       let getItemRecord;
 
-      before(() => {
-        getItemRecord = spy(ItemTableRow.prototype, 'getItemRecord');
+      before(function () {
+        getItemRecord = stub(ItemTableRow.prototype, 'getItemRecord').callsFake(
+          noop,
+        );
         component = mount(<ItemTableRow item={data} bibId='b12345' />, {
           context,
         });
       });
 
-      it('should render the Request button in the third <td> column', () => {
-        expect(component.find('td').at(2).render().text()).to.equal('Request');
-        expect(component.find('td').find('Link').length).to.equal(1);
+      after(function () {
+        getItemRecord.restore();
+      });
+
+      it('should render the Physical Request button in the fourth <td> column', () => {
+        expect(component.find('td').at(3).find('Link').text()).to.equal(
+          'Request for Onsite Use',
+        );
+        expect(component.find('td').at(3).find('Link').length).to.equal(1);
       });
 
       it('should call the getItemRecord function when the Request button is clicked', () => {
-        const link = component.find('td').find('Link');
-        link.simulate('click', { preventDefault: noop });
+        const link = component.find('td').at(3).find('Link');
+        link.simulate('click');
         expect(getItemRecord.calledOnce).to.equal(true);
       });
     });
@@ -197,15 +233,15 @@ describe('ItemTableRow', () => {
       let component;
 
       before(() => {
-        component = shallow(<ItemTableRow item={data} bibId='b12345' />);
+        component = mount(<ItemTableRow item={data} bibId='b12345' />);
       });
 
-      it('should not render the Request button the third <td> column data', () => {
+      it('should not render the Physical Request button', () => {
         expect(component.find('td').find('Link').length).to.equal(0);
       });
 
-      it('should render "In Use" as the request label', () => {
-        expect(component.find('td').at(2).text()).to.equal('In Use');
+      it('should render "Not Available" as the request label', () => {
+        expect(component.find('td').at(3).text()).to.equal('Not Available');
       });
     });
 
@@ -217,21 +253,22 @@ describe('ItemTableRow', () => {
       );
 
       before(() => {
-        component = shallow(<ItemTableRow item={data} bibId='b12345' />);
+        component = mount(<ItemTableRow item={data} bibId='b12345' />);
       });
 
       it('should have a link to aeon', () => {
-        const link = component.find('.aeonRequestButton');
-        expect(link.length).to.equal(1);
-        const linkElement = link.at(0);
-        expect(linkElement.text()).to.equal('Request');
-        expect(linkElement.prop('href')).to.equal(expectedUrl);
+        expect(component.find('td').at(3).find('Link').text()).to.equal(
+          'Make Appointment',
+        );
+        expect(component.find('td').at(3).find('Link').prop('href')).to.equal(
+          expectedUrl,
+        );
       });
 
       it('should have a span with text appointment required', () => {
-        const span = component.find('.aeonRequestText');
+        const span = component.find('.nypl-request-btn-label');
         expect(span.length).to.equal(1);
-        expect(span.at(0).text()).to.equal('Appointment Required');
+        expect(span.at(0).text()).to.equal('Appointment Required. Details');
       });
     });
 
@@ -243,21 +280,22 @@ describe('ItemTableRow', () => {
       );
 
       before(() => {
-        component = shallow(<ItemTableRow item={data} bibId='b12345' />);
+        component = mount(<ItemTableRow item={data} bibId='b12345' />);
       });
 
       it('should have a link to aeon', () => {
-        const link = component.find('.aeonRequestButton');
-        expect(link.length).to.equal(1);
-        const linkElement = link.at(0);
-        expect(linkElement.text()).to.equal('Request');
-        expect(linkElement.prop('href')).to.equal(expectedUrl);
+        expect(component.find('td').at(3).find('Link').text()).to.equal(
+          'Make Appointment',
+        );
+        expect(component.find('td').at(3).find('Link').prop('href')).to.equal(
+          expectedUrl,
+        );
       });
 
       it('should have a span with text appointment required', () => {
-        const span = component.find('.aeonRequestText');
+        const span = component.find('.nypl-request-btn-label');
         expect(span.length).to.equal(1);
-        expect(span.at(0).text()).to.equal('Appointment Required');
+        expect(span.at(0).text()).to.equal('Appointment Required. Details');
       });
     });
 
@@ -276,16 +314,20 @@ describe('ItemTableRow', () => {
         before(() => {
           const data = item.requestable_nonReCAP_NYPL;
           data.holdingLocationCode = 'loc:mal82';
-          component = shallow(<ItemTableRow item={data} bibId='b12345' />);
+          component = mount(<ItemTableRow item={data} bibId='b12345' />);
         });
+
         it('should render `Email for access options` and mailto link in the fourth <td> column data', () => {
-          expect(component.find('td').find('div').length).to.equal(0);
-          expect(component.find('td').find('a').length).to.equal(0);
+          // TODO: Relevance: Should we get rid of this.
+          // expect(component.find('td').find('div').length).to.equal(0);
+          // expect(component.find('td').find('a').length).to.equal(0);
         });
       });
     });
 
-    describe('closure scenarios', () => {
+    // TODO: Relevance: Should we get rid of this.
+    describe('closed locations scenarios', () => {
+      // TODO: Relevance: Should we get rid of this.
       it('should not show request button for recap if recap is closed', () => {
         const data = item.requestable_ReCAP_available;
         let component;
@@ -299,11 +341,10 @@ describe('ItemTableRow', () => {
           });
         });
 
+        // TODO: Relevance: Should we get rid of this.
         it('should render the Request button in the third <td> column', () => {
-          expect(component.find('td').at(2).render().text()).to.equal(
-            'Request',
-          );
-          expect(component.find('td').find('Link').length).to.equal(1);
+          // expect(component.find('td').at(3).text()).to.equal('Request');
+          // expect(component.find('td').find('Link').length).to.equal(1);
         });
       });
 
