05e1021f46bcd60ee3f945942c3ff0cd697148a7
wormjim <gcooled29@gmail.com>
2022-03-28 12:24:22 -0400

    Add Missing PropTypes Import

--- src/app/components/BibPage/BottomBibDetails.jsx
+++ src/app/components/BibPage/BottomBibDetails.jsx
@@ -1,4 +1,5 @@
 import { Heading } from '@nypl/design-system-react-components';
+import PropTypes from 'prop-types';
 import React from 'react';
 import { annotatedMarcDetails } from '../../utils/bibDetailsUtils';
 import { isNyplBnumber } from '../../utils/utils';
