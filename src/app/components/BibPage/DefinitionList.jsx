e8add21012667c01bf1cb2c5f468d9c690ca821d
wormjim <gcooled29@gmail.com>
2022-04-01 10:58:34 -0400

    Refactor Definition List to Simplify Reading

--- src/app/components/BibPage/DefinitionList.jsx
+++ src/app/components/BibPage/DefinitionList.jsx
@@ -7,28 +7,29 @@ import SubjectHeadings from './SubjectHeadings';
  * Expects data in the form of [{ term: '', definition: '' }, {...}, ...].
  */
 const DefinitionList = ({ data, headings }) => {
-  const getDefinitions = (definitions) =>
-    definitions.map((item) => {
-      if (!item || (!item.term && !item.definition)) {
-        return null;
-      }
-
-      if (item.term === 'Subject' && headings)
-        return <SubjectHeadings key='subjects' headings={headings} />;
-
-      return [
-        <dt key={`term-${item.term}`}>{item.term}</dt>,
-        <dd data={`definition-${item.term}`} key={`definition-${item.term}`}>
-          {item.definition}
-        </dd>,
-      ];
-    });
-
   if (!data || !data.length) {
     return null;
   }
 
-  return <dl>{getDefinitions(data)}</dl>;
+  return (
+    <dl>
+      {data.map((item) => {
+        if (!item || (!item.term && !item.definition)) {
+          return null;
+        }
+
+        if (item.term === 'Subject' && headings)
+          return <SubjectHeadings key='subjects' headings={headings} />;
+
+        return [
+          <dt key={`term-${item.term}`}>{item.term}</dt>,
+          <dd data={`definition-${item.term}`} key={`definition-${item.term}`}>
+            {item.definition}
+          </dd>,
+        ];
+      })}
+    </dl>
+  );
 };
 
 DefinitionList.propTypes = {
