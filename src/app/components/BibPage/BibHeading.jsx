47e77f243ca24b0f34b070c4ceeab1164240f634
wormjim <gcooled29@gmail.com>
2022-04-04 11:33:46 -0400

    Create BibHeading Component
    Extracted Logic form the BibPage relating to the heading (Non NYPL Specific head) of the page. Title and back to search results are related to the heading. This helps simplfiy logic related to specifc Container Components such as Header, Footer, and Body.

---
+++ src/app/components/BibPage/BibHeading.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useBib } from '../../context/Bib.Provider';
+import ParallelsFields from '../Parallels/ParallelsFields';
+import BackToSearchResults from './BackToSearchResults';
+
+const BibHeading = ({ searched }) => {
+  const {
+    bib: { title = ' ' },
+    bibId,
+  } = useBib();
+
+  return (
+    <section className='nypl-item-details__heading'>
+      <ParallelsFields headingLevel={2} pField={'title'}>
+        {title[0]}
+      </ParallelsFields>
+
+      <BackToSearchResults result={searched} bibId={bibId} />
+    </section>
+  );
+};
+
+BibHeading.propTypes = {
+  searched: PropTypes.object,
+};
+
+export default BibHeading;
