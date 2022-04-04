820b8054536e5baf9efd4c88fe42dbc6ab00f9cd
wormjim <gcooled29@gmail.com>
2022-04-04 11:31:12 -0400

    WIP: Define Logic For Use Parallel
    This is the first iteration of how we may find this hook useful.

--- src/app/context/Bib.Provider.jsx
+++ src/app/context/Bib.Provider.jsx
@@ -1,9 +1,24 @@
 import PropTypes from 'prop-types';
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 import BibContext from './Bib.Context';
 
 const BibProvider = ({ bib, children }) => {
-  return <BibContext.Provider value={bib}>{children}</BibContext.Provider>;
+  // This is pointless due to redux.
+  // The redux store bib is updated on the bib page
+  // With an entirley new object. This causes a rerender
+  // with a new bib which causes useMemo to be re-executed.
+  // TODO: Would useState be better here?
+  // Do we care about a single rerender?
+  // As of now there are only two renders.
+  const parallels = useMemo(() => extractParallels(bib), [bib]);
+
+  const bibId = bib['@id'] ? bib['@id'].substring(4) : '';
+
+  return (
+    <BibContext.Provider value={{ bib, parallels, bibId }}>
+      {children}
+    </BibContext.Provider>
+  );
 };
 
 BibProvider.propTypes = {
@@ -22,7 +37,18 @@ const useBib = () => {
 };
 
 const useBibParallel = (field = '') => {
+  const { bib, parallels = {} } = useBib();
+  const hasParallels = !!Object.keys(parallels).length && !!parallels[field];
+
   // WIP
+  // Possible constructs
+  // return [['asdfjasd', 'ba;ba;'], ['adfasdf', '']]
+  return {
+    bib,
+    hasParallels,
+    field: bib[field],
+    parallel: parallels[field],
+  };
 };
 
 const WithBib = (Component) => {
@@ -38,3 +64,21 @@ const WithBib = (Component) => {
 export { useBib, useBibParallel, BibProvider, WithBib };
 
 export default BibProvider;
+
+function extractParallels(bib) {
+  return (
+    Object.keys(bib).reduce((store, key) => {
+      if (key.includes('parallel')) {
+        const field = key.slice('parallel'.length);
+        const match = field.charAt(0).toLocaleLowerCase() + field.slice(1);
+
+        return {
+          ...store,
+          [match]: { original: bib[match], paralell: bib[key] },
+        };
+      }
+
+      return store;
+    }, {}) || {}
+  );
+}
