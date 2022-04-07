842f4c6366d4efd3f8052b636bb7a647607b4aca
wormjim <gcooled29@gmail.com>
2022-04-07 17:31:41 -0400

    Filter Out Empty Values From List

--- src/app/components/BibPage/components/DefinitionField.jsx
+++ src/app/components/BibPage/components/DefinitionField.jsx
@@ -23,54 +23,55 @@ const DefinitionField = ({ field, bibValues = [], additional = false }) => {
 
           const element = { value };
 
-        if (field.linkable) {
-          element.value = (
-            <LinkableBibField
-              label={field.label}
-              field={field.value}
-              bibValue={value}
-              outbound={field.selfLinkable}
-            />
-          );
-        }
+          if (field.linkable) {
+            element.value = (
+              <LinkableBibField
+                label={field.label}
+                field={field.value}
+                bibValue={value}
+                outbound={field.selfLinkable}
+              />
+            );
+          }
 
-        const definition =
-          element.value.prefLabel ?? element.value.label ?? element.value;
+          const definition =
+            element.value.prefLabel ?? element.value.label ?? element.value;
 
-        return <li key={`${value}-${idx}`}>{definition}</li>;
+          return <li key={`${value}-${idx}`}>{definition}</li>;
 
-        // const url = `filters[${field.value}]=${value['@id']}`;
+          // const url = `filters[${field.value}]=${value['@id']}`;
 
-        // let itemValue = field.linkable ? (
-        //   <Link
-        //     onClick={(event) =>
-        //       this.newSearch(event, url, field.value, value['@id'], field.label)
-        //     }
-        //     to={`${appConfig.baseUrl}/search?${url}`}
-        //   >
-        //     {value.prefLabel}
-        //   </Link>
-        // ) : (
-        //   <span>{value.prefLabel}</span>
-        // );
-        // if (field.selfLinkable) {
-        //   itemValue = (
-        //     <a
-        //       href={value['@id']}
-        //       onClick={() =>
-        //         trackDiscovery(
-        //           'Bib fields',
-        //           `${field.label} - ${value.prefLabel}`,
-        //         )
-        //       }
-        //     >
-        //       {value.prefLabel}
-        //     </a>
-        //   );
-        // }
+          // let itemValue = field.linkable ? (
+          //   <Link
+          //     onClick={(event) =>
+          //       this.newSearch(event, url, field.value, value['@id'], field.label)
+          //     }
+          //     to={`${appConfig.baseUrl}/search?${url}`}
+          //   >
+          //     {value.prefLabel}
+          //   </Link>
+          // ) : (
+          //   <span>{value.prefLabel}</span>
+          // );
+          // if (field.selfLinkable) {
+          //   itemValue = (
+          //     <a
+          //       href={value['@id']}
+          //       onClick={() =>
+          //         trackDiscovery(
+          //           'Bib fields',
+          //           `${field.label} - ${value.prefLabel}`,
+          //         )
+          //       }
+          //     >
+          //       {value.prefLabel}
+          //     </a>
+          //   );
+          // }
 
-        // return <li key={value.prefLabel}>{itemValue}</li>;
-      })}
+          // return <li key={value.prefLabel}>{itemValue}</li>;
+        })
+        .filter(Boolean)}
     </ul>
   );
 };
