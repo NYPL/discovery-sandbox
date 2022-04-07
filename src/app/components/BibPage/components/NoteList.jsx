d4faec3c737e89f887b16f1f8158020c8aa46a99
wormjim <gcooled29@gmail.com>
2022-04-07 17:39:10 -0400

    Process && Display Notes w/ the Parallel

--- src/app/components/BibPage/components/NoteList.jsx
+++ src/app/components/BibPage/components/NoteList.jsx
@@ -1,18 +1,45 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useBibParallel } from '../../../context/Bib.Provider';
 
-const NoteList = ({ group, type }) => {
+const NoteList = ({ notes, type }) => {
+  const {
+    hasParallels,
+    field: { mapping = [] },
+  } = useBibParallel('note');
+
+  const list = (hasParallels && mapping) || notes;
+
   return (
     <ul>
-      {group[type].map((note, idx) => (
-        <li key={idx.toString()}>{note.prefLabel}</li>
-      ))}
+      {list
+        .map((note, idx, ogArr) => {
+          if (type === 'Note') {
+            return <li key={idx.toString()}>{note.prefLabel ?? note}</li>;
+          }
+
+          // Check by object reference
+          if (note === notes[0]) {
+            return (
+              <>
+                {(typeof ogArr[idx - 1] === 'string' && (
+                  <li key={(idx - 1).toString()}>{ogArr[idx - 1]}</li>
+                )) ||
+                  null}
+                <li key={idx.toString()}>{note.prefLabel ?? note}</li>
+              </>
+            );
+          }
+
+          return null;
+        })
+        .filter(Boolean)}
     </ul>
   );
 };
 
 NoteList.propTypes = {
-  group: PropTypes.object,
+  notes: PropTypes.array,
   type: PropTypes.string,
 };
 
