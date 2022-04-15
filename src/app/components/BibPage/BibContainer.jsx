321bbdb605e869517fcd865ca1a1e264587b19c7
wormjim <gcooled29@gmail.com>
2022-04-15 13:12:38 -0400

    Rename Variable AggregetedElectronicResources
    Renamed the varaible from AggregetedElectronicResources to Resources

--- src/app/components/BibPage/BibContainer.jsx
+++ src/app/components/BibPage/BibContainer.jsx
@@ -17,24 +17,19 @@ const BibContainer = ({ location, selection, keywords }) => {
   const { bib, bibId } = useBib();
 
   const items = (bib.checkInItems || []).concat(LibraryItem.getItems(bib));
-  const aggregatedElectronicResources = getAggregatedElectronicResources(items);
+  const resources = getAggregatedElectronicResources(items);
 
   return (
     <>
       <BibHeading searched={selection} />
 
-      <TopBibDetails
-        resources={pluckAeonLinksFromResource(
-          aggregatedElectronicResources,
-          items,
-        )}
-      />
+      <TopBibDetails resources={pluckAeonLinksFromResource(resources, items)} />
 
       <BibItems items={items} location={location} keywords={keywords} />
 
       <BibHoldings />
 
-      <BottomBibDetails bib={bib} resources={aggregatedElectronicResources} />
+      <BottomBibDetails bib={bib} resources={resources} />
 
       <LegacyCatalogLink recordNumber={bibId} />
     </>
