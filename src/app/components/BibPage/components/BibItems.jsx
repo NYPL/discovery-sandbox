f691862fc4b77451627bca5178f5694a95275d01
wormjim <gcooled29@gmail.com>
2022-04-15 13:14:44 -0400

    Add Missing PropTypes Import

--- src/app/components/BibPage/components/BibItems.jsx
+++ src/app/components/BibPage/components/BibItems.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { useMemo } from 'react';
 import { useBib } from 'src/app/context/Bib.Provider';
 import { isElectronic } from 'src/app/utils/utils';
