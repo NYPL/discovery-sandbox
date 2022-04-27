e8d167aaba10540aa42d42d233c3816cbf1e240c
wormjim <gcooled29@gmail.com>
2022-04-27 12:11:41 -0400

    Remove Use Memo From BibDetails
    The use of useMemo here was not very valuable and made no impact on the performace of the application. It was removed.

--- src/app/components/BibPage/BibDetails.jsx
+++ src/app/components/BibPage/BibDetails.jsx
@@ -19,71 +19,64 @@ const BibDetails = ({ fields = [], resources = [], marcs }) => {
     parallels,
   } = useBibParallel();
 
-  // This does not Memoize due to Redux setting a New Bib Item
-  // Potential for Improvement
-  // TODO: Make ticket
-  const definitions = useMemo(() => {
-    // Loops through fields and builds the Definition Field Component
-    return fields.reduce((store, field) => {
-      const value =
-        bib[field.value] ??
-        // Allow origin to be resources
-        (field.label === 'Electronic Resource' &&
-          resources.length &&
-          resources);
+  // Loops through fields and builds the Definition Field Component
+  const definitions = fields.reduce((store, field) => {
+    const value =
+      bib[field.value] ??
+      // Allow origin to be resources
+      (field.label === 'Electronic Resource' && resources.length && resources);
 
-      if (field.value === 'note' && value) {
-        // INVESTIGATE:
-        // Can we avoid having to loop here?
-        // Although unlikely what happens at groups of 10, 20, ...100
-        const paras = (parallels['note'] && parallels['note'].parallel) || [];
-        const group = groupNotesBySubject(setParallelToNote(value, paras));
+    if (field.value === 'note' && value) {
+      // INVESTIGATE:
+      // Can we avoid having to loop here?
+      // Although unlikely what happens at groups of 10, 20, ...100
+      const paras = (parallels['note'] && parallels['note'].parallel) || [];
+      const group = groupNotesBySubject(setParallelToNote(value, paras));
 
-        return [
-          ...store,
-          // In order to get the noteType as a label
-          // we need to process this here
-          ...Object.entries(group).map(([label, notes]) => {
-            // type notes = Note[]
-            return {
-              // term is the label of the feild
-              term: label,
-              // definition is the value of the label
-              definition: <DefinitionNoteField values={notes} />,
-              // TODO: Can we use the DefinitionField instead of DefinitionNoteField?
-              // definition: <DefinitionField bibValues={notes} field={field} />,
-            };
-          }),
-        ];
-      }
+      return [
+        ...store,
+        // In order to get the noteType as a label
+        // we need to process this here
+        ...Object.entries(group).map(([label, notes]) => {
+          // type notes = Note[]
+          return {
+            // term is the label of the feild
+            term: label,
+            // definition is the value of the label
+            definition: <DefinitionNoteField values={notes} />,
+            // TODO: Can we use the DefinitionField instead of DefinitionNoteField?
+            // definition: <DefinitionField bibValues={notes} field={field} />,
+          };
+        }),
+      ];
+    }
 
-      if (value) {
-        const ident = validIdentifier(field, value);
-        // To avoid adding a label with empty array for identifiers
-        if (ident && !ident.length) return store;
+    if (value) {
+      const ident = validIdentifier(field, value);
+      // To avoid adding a label with empty array for identifiers
+      if (ident && !ident.length) return store;
 
-        return [
-          ...store,
-          {
-            term: field.label,
-            definition: (
-              <DefinitionField
-                bibValues={ident ?? value}
-                field={field}
-                // TODO: This is not correct
-                // Additional checks for stying changes
-                // It is suppose to style additional marcs
-                // We do not apply the marcs here
-                additional={!!marcs.length}
-              />
-            ),
-          },
-        ];
-      }
+      return [
+        ...store,
+        {
+          term: field.label,
+          definition: (
+            <DefinitionField
+              bibValues={ident ?? value}
+              field={field}
+              // TODO: This is not correct
+              // Additional checks for stying changes
+              // It is suppose to style additional marcs
+              // We do not apply the marcs here
+              additional={!!marcs.length}
+            />
+          ),
+        },
+      ];
+    }
 
-      return store;
-    }, []);
-  }, [bib, fields, marcs.length, parallels, resources]);
+    return store;
+  });
 
   // Make sure fields is a nonempty array
   if (_isEmpty(fields) || !_isArray(fields)) {
