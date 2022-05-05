08caac60b06b510d1fd9b6682709584fa098a81b
wormjim <gcooled29@gmail.com>
2022-05-05 10:30:54 -0400

    Correct Prettier Config && Run (#1875)
    Corrected the file name of the Prettier config. Should not have been proper case.

--- src/app/pages/HoldRequest.jsx
+++ src/app/pages/HoldRequest.jsx
@@ -318,7 +318,9 @@ export class HoldRequest extends React.Component {
         : '';
     const itemId = params && params.itemId ? params.itemId : '';
     const selectedItem = bib && itemId ? LibraryItem.getItem(bib, itemId) : {};
-    const selectedItemAvailable = selectedItem ? selectedItem.physRequestable : false;
+    const selectedItemAvailable = selectedItem
+      ? selectedItem.physRequestable
+      : false;
     const bibLink =
       bibId && title ? (
         <h2>
