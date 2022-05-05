29ca271acdde901813f38dcbfed1db17c93dea70
Vera Kahn <verakahn@nypl.org>
2022-05-05 10:13:42 -0400

    update tests

--- test/unit/Bib.test.js
+++ test/unit/Bib.test.js
@@ -58,15 +58,15 @@ describe('Bib', () => {
 
   describe('appendDimensionsToExtent', () => {
     it('should add a semicolon after extent if there is not one already', () => {
-      const newExtent = Bib.appendDimensionsToExtent(mockBib)
+      const [newExtent] = Bib.appendDimensionsToExtent(mockBib)
       expect(newExtent).to.include('; ')
     })
     it('should append dimensions to extent', () => {
-      const newExtent = Bib.appendDimensionsToExtent(mockBib)
+      const [newExtent] = Bib.appendDimensionsToExtent(mockBib)
       expect(newExtent).to.equal('99 bottles of beer; 99 x 99 cm')
     })
     it('should not add semicolon if it already is in extent', () => {
-      const newExtent = Bib.appendDimensionsToExtent({extent: '700 sheets of woven gold; ', dimensions: '1 x 1 in.'})
+      const [newExtent] = Bib.appendDimensionsToExtent({extent: '700 sheets of woven gold; ', dimensions: '1 x 1 in.'})
       expect(newExtent).to.equal('700 sheets of woven gold; 1 x 1 in.')
     })
   })
