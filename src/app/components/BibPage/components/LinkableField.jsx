15956fe6fc4a0496b6821cf8f0d88304c2fdc0de
Vera Kahn <verakahn@nypl.org>
2022-05-10 16:09:05 -0400

    change logic to use map

--- src/app/components/BibPage/components/LinkableField.jsx
+++ src/app/components/BibPage/components/LinkableField.jsx
@@ -19,11 +19,12 @@ const LinkableBibField = ({
     ? bibValue.prefLabel || bibValue.label || bibValue.url
     : bibValue;
 
+const partialUrlEncodedBibValue = bibValue.split('').map((char) => char === '&' ? '%26' : char === '+' ? '%2B' : char).join('')
   // react router url encoding ignores & and +, so such characters within bibValue need to be encoded manually
   const filter = `filters[${field}]=${
     filterPath ??
     bibValue['@id'] ??
-    bibValue.replace('&', '%26').replace('+', '%2B') ??
+    partialUrlEncodedBibValue ??
     ''
   }`;
 
