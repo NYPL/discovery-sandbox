db562795105689b515f750cc3da7a07e6c39778c
Vera Kahn <verakahn@nypl.org>
2022-05-11 13:47:02 -0400

    add url encoding for & +

--- src/app/components/BibPage/BibDetails.jsx
+++ src/app/components/BibPage/BibDetails.jsx
@@ -195,6 +195,7 @@ class BibDetails extends React.Component {
 
     if (bibValues.length === 1) {
       const bibValue = bibValues[0];
+
       const url = `filters[${fieldValue}]=${bibValue}`;
       return this.getDefinitionOneItem(
         bibValue,
@@ -211,7 +212,18 @@ class BibDetails extends React.Component {
     return (
       <ul>
         {bibValues.map((value) => {
-          const url = `filters[${fieldValue}]=${value}`;
+          let partialUrlEncodedBibValue;
+          if (typeof value === 'string') {
+            partialUrlEncodedBibValue = value
+              .split('')
+              .map((char) =>
+                char === '&' ? '%26' : char === '+' ? '%2B' : char,
+              )
+              .join('');
+          }
+          const url = `filters[${fieldValue}]=${
+            partialUrlEncodedBibValue || value
+          }`;
           return (
             <li
               key={`filter${fieldValue}${
