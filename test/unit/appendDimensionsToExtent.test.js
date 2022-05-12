ebff9c1588acba2b0e041f4959e756389ce30fc6
Vera Kahn <verakahn@nypl.org>
2022-05-12 13:35:54 -0400

    update tests for new code changes

--- test/unit/appendDimensionsToExtent.test.js
+++ test/unit/appendDimensionsToExtent.test.js
@@ -2,27 +2,28 @@ import { expect } from 'chai';
 import { appendDimensionsToExtent } from '../../src/app/utils/appendDimensionsToExtent'
 
 describe('appendDimensionsToExtent', () => {
-  const mockBib = { extent: ['99 bottles of beer'], dimensions: ['99 x 99 cm']}
   it('should add a semicolon after extent if there is not one already', () => {
-    const [newExtent] = appendDimensionsToExtent(mockBib)
-    expect(newExtent).to.include('; ')
+    const bib = { extent: ['99 bottles of beer'], dimensions: ['99 x 99 cm']}
+    appendDimensionsToExtent(bib)
+    expect(bib.extent[0]).to.include('; ')
   })
   it('should append dimensions to extent', () => {
-    const [newExtent] = appendDimensionsToExtent(mockBib)
-    expect(newExtent).to.equal('99 bottles of beer; 99 x 99 cm')
+    const bib = { extent: ['99 bottles of beer'], dimensions: ['99 x 99 cm']}
+    appendDimensionsToExtent(bib)
+    expect(bib.extent[0]).to.equal('99 bottles of beer; 99 x 99 cm')
   })
   it('should not add semicolon if it already is in extent', () => {
-    const [newExtent] = appendDimensionsToExtent({ extent: ['700 sheets of woven gold; '], dimensions: ['1 x 1 in.'] })
-    expect(newExtent).to.equal('700 sheets of woven gold; 1 x 1 in.')
+    const bib = appendDimensionsToExtent({ extent: ['700 sheets of woven gold; '], dimensions: ['1 x 1 in.'] })
+    expect(bib.extent[0]).to.equal('700 sheets of woven gold; 1 x 1 in.')
   })
   it('should remove semicolon if there is no dimensions', () => {
-    const [newExtent] = appendDimensionsToExtent({ extent: ['700 sheets of woven gold; '] })
-    const [anotherExtent] = appendDimensionsToExtent({ extent: ['700 sheets of woven gold;'] })
-    expect(newExtent).to.equal('700 sheets of woven gold')
-    expect(anotherExtent).to.equal('700 sheets of woven gold')
+    const bib = appendDimensionsToExtent({ extent: ['700 sheets of woven gold; '] })
+    const anotherBib = appendDimensionsToExtent({ extent: ['700 sheets of woven gold;'] })
+    expect(bib.extent[0]).to.equal('700 sheets of woven gold')
+    expect(anotherBib.extent[0]).to.equal('700 sheets of woven gold')
   })
-  it('should return undefined if there is no extent', () => {
-    const nullExtent = appendDimensionsToExtent({})
-    expect(nullExtent).to.equal(undefined)
+  it('should do nothing if there is no extent', () => {
+    const bib = appendDimensionsToExtent({ dimensions: 'lol' })
+    expect(bib).to.not.have.keys('extent')
   })
 })
