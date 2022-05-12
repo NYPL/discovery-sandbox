50967c8bc854cb44d87c9fc7cedb1ae87ac0bb02
Vera Kahn <verakahn@nypl.org>
2022-05-12 13:17:46 -0400

    change approach to use regex

--- src/app/utils/appendDimensionsToExtent.js
+++ src/app/utils/appendDimensionsToExtent.js
@@ -1,18 +1,9 @@
 export const appendDimensionsToExtent = (bib) => {
-  if (!bib.extent || bib.extent.length === 0) return
-  let extent = bib.extent[0]
-  let punctuationToAdd = ''
-  // Check if extent was cataloged with a semicolon already at the end:
-  const semicolon = (extent.slice(-2) === '; ' || extent.slice(-1) === ';')
-  if (semicolon) {
-    if (extent.slice(-1) !== ' ') punctuationToAdd += ' '
-  } else punctuationToAdd = '; '
-  if (bib.dimensions && bib.dimensions[0].length) {
-    // If there is a dimensions field, append  it to the extent and make sure they are separated by a semicolon and a space:
-    extent = extent + punctuationToAdd + bib.dimensions[0]
-  } else {
-    // If there is no dimensions field, remove the semicolon
-    extent = punctuationToAdd.length === 0 ? extent.slice(0, -2) : extent.slice(0, -1)
+    if (!bib.extent || bib.extent.length === 0 || !bib.extent[0]) return;
+    
+    let parts = [ bib.extent[0].replace(/\s*;\s*$/, '') ]
+    if (bib.dimensions && bib.dimensions[0]) parts.push(bib.dimensions[0])
+    
+    return [parts.join('; ')]
   }
-  return [extent]
-}
+
