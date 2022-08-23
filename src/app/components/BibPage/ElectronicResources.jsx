b92dfb32d4a1a8c86559650a368359df0354a089
Vera Kahn <verakahn@nypl.org>
2022-08-23 12:43:06 -0400

    create collapsing electronic resources button

--- src/app/components/BibPage/ElectronicResources.jsx
+++ src/app/components/BibPage/ElectronicResources.jsx
@@ -1,22 +1,44 @@
-import { Card, CardContent, CardHeading } from '@nypl/design-system-react-components'
+import { Card, CardContent, CardHeading, Button, Icon } from '@nypl/design-system-react-components'
 import generateERLinksList from '../../utils/electronicResources'
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 /**
  * ElectronicResources renders a list of electronic resources links, sans aeon links
  * @param {array} electronicResources - an array of electronic resources, passed in as a prop from the BibPage component
  */
+
+
 const ElectronicResources = ({ electronicResources }) => {
+  const allResources = generateERLinksList(electronicResources)
+  const threeResources = generateERLinksList(electronicResources.slice(0, 3))
+  const more = `all ${electronicResources.length}`
+  const less = 'fewer'
+
+  const [showMore, setShowMore] = useState(true)
+
+  const scrollToRef = useRef();
+
+  const onClick = () => {
+    setShowMore((prev) => {
+      scrollToRef.current.scrollIntoView()
+      return !prev
+    })
+  }
+
   if (!electronicResources || !electronicResources.length) {
     return null;
   }
-  const resources = generateERLinksList(electronicResources)
-  return (<Card isBordered padding="16px">
+  return (<Card ref={scrollToRef} isBordered padding="16px">
     <CardHeading level="three" id="no-img1-heading1">
       Available Online
     </CardHeading>
     <CardContent>
-      {resources}
+      {showMore ? threeResources : allResources}
+      {electronicResources.length > 3 ?
+        <Button style={{ textDecoration: 'none' }} isBordered='false' id='see-more-button' onClick={onClick} buttonType='link'>
+          See {showMore ? more : less} resources
+          <Icon style={{ marginLeft: '5px' }} iconRotation={`rotate${showMore ? 0 : 180}`} name="arrow" size="small" />
+        </Button> : null}
     </CardContent>
   </Card>)
 }
