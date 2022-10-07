e4036f4ddf19ef5af2ea989b75790191e32921bc
Vera Kahn <verakahn@nypl.org>
2022-10-07 14:08:34 -0400

    ditto last commit

--- src/app/components/Item/RequestInfo.jsx
+++ src/app/components/Item/RequestInfo.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import { } from '@nypl/design-system-react-components';
 import { isEmpty as _isEmpty } from 'underscore';
@@ -16,9 +15,6 @@ const RequestInfo = ({ isRecap, isAeon, available, division, locationUrl }) => {
   if (available) {
     if (isRecap) {
       return <Link
-        style={{
-          fontSize: 'var(--nypl-fontSizes--3)'
-        }}
         href='https://www.nypl.org/help/request-research-materials'>
         How do I pick up this item and when will it be ready?
       </Link>
