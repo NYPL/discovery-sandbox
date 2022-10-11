97a1e9baa3206ac7f53ba8f3ba676d1533154972
danamansana <danielappel@nypl.org>
2022-10-11 14:33:25 -0400

    Add separate tbody for each item

--- src/app/components/Item/ItemTable.jsx
+++ src/app/components/Item/ItemTable.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { isArray as _isArray } from 'underscore';
+import { isArray as _isArray, isEmpty as _isEmpty } from 'underscore';
 
 import ItemTableRow from './ItemTableRow';
 
@@ -17,31 +17,45 @@ const ItemTable = ({ items, holdings, bibId, id, searchKeywords, page }) => {
     items.some(item => item.volume && item.volume.length) && page !== 'SearchResults'
   );
 
+  const itemGroups = (page === 'SearchResults' ?
+    items.filter(item =>
+      !(_isEmpty(item) || item.isElectronicResource)
+    ).map(item =>
+      [item]
+    ) :
+    [items]
+  );
+
   return (
     <table className="nypl-basic-table" id={id}>
-      <caption className="hidden">Item details</caption>
-      <thead>
-        <tr>
-          {includeVolColumn ? <th scope="col">Vol/Date</th> : null}
-          <th scope="col">Format</th>
-          <th scope="col">Call Number</th>
-          <th scope="col">Location</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          items.map(item =>
-            (<ItemTableRow
-              key={item.id}
-              item={item}
-              bibId={bibId}
-              searchKeywords={searchKeywords}
-              includeVolColumn={includeVolColumn}
-              page={page}
-            />),
+    <caption className="hidden">Item details</caption>
+      {
+        itemGroups.map(group => (
+          <>
+            <tbody>
+              <tr>
+                {includeVolColumn ? <th scope="col">Vol/Date</th> : null}
+                <th scope="col">Format</th>
+                <th scope="col">Call Number</th>
+                <th scope="col">Location</th>
+              </tr>
+              {
+                group.map(item =>
+                  (<ItemTableRow
+                    key={item.id}
+                    item={item}
+                    bibId={bibId}
+                    searchKeywords={searchKeywords}
+                    includeVolColumn={includeVolColumn}
+                    page={page}
+                    />),
+                  )
+                }
+              </tbody>
+            </>
           )
-        }
-      </tbody>
+        )
+      }
     </table>
   );
 };
