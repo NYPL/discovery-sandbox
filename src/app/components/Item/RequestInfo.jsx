a76fc146ced50c2b965f1518db75fddda9aaa7dc
Vera Kahn <verakahn@nypl.org>
2022-10-11 16:32:06 -0400

    add location link

--- src/app/components/Item/RequestInfo.jsx
+++ src/app/components/Item/RequestInfo.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { Text, Link } from '@nypl/design-system-react-components';
 import { isEmpty as _isEmpty } from 'underscore';
 
+const locationUrlEndpoint = (location) => {
+  const loc = location.split(' ')[0]
+  const urls = {
+    'Schwarzman': 'schwarzman',
+    'Library': 'lpa',
+    'Schomburg': 'schomburg'
+  }
+  return urls[loc]
+}
+
 const RequestInfo = ({ isRecap, aeonUrl, available, divisionUrl, dueDate, location }) => {
   if (available) {
     if (isRecap) {
@@ -20,16 +30,19 @@ const RequestInfo = ({ isRecap, aeonUrl, available, divisionUrl, dueDate, locati
     }
     else {
       //available onsite item
+      const locationShort = location.split('-')[0]
       return <Text className='availability-alert'>
         <span className='available-text'>Available </span>
-        - Can be used on site. Please visit [LOCATION LINK] to submit a request in person</Text>
+        - Can be used on site. Please visit <Link href={'https://www.nypl.org/locations/' + locationUrlEndpoint(location)}>New York Public Library - {locationShort}</Link> to submit a request in person</Text>
     }
   } else {
     const dueDateAlert = <span>{` - In use until ${dueDate}`}</span>
     return <div className='availability-alert'>
       <span className='unavailable-text'>Not available</span>
       {dueDate && dueDateAlert}
-      <Link href='https://www.nypl.org/about/divisions'>. See a librarian for assistance</Link>
+      <span> - Please
+        <Link href='https://www.nypl.org/about/divisions'> contact a librarian </Link>
+        for assistance</span>
     </div>
   }
 }
