c65910653df18fb6ba61a8a69696360a838368f9
Vera Kahn <verakahn@nypl.org>
2022-10-12 15:31:30 -0400

    remove external type

--- src/app/components/Item/RequestInfo.jsx
+++ src/app/components/Item/RequestInfo.jsx
@@ -17,7 +17,6 @@ const RequestInfo = ({ isRecap, aeonUrl, available, divisionUrl, dueDate, locati
     if (isRecap) {
       //available recap item
       return <Link
-        type='external'
         className='info-link'
         href='https://www.nypl.org/help/request-research-materials'>
         How do I pick up this item and when will it be ready?
@@ -26,7 +25,7 @@ const RequestInfo = ({ isRecap, aeonUrl, available, divisionUrl, dueDate, locati
       return <Text className='availability-alert'>
         <span className='available-text'>Available by appointment</span>
         <span> at </span>
-        <Link type='external' className='division-link' href={divisionUrl}>{location}</Link>
+        <Link className='division-link' href={divisionUrl}>{location}</Link>
       </Text>
     }
     else {
@@ -36,7 +35,7 @@ const RequestInfo = ({ isRecap, aeonUrl, available, divisionUrl, dueDate, locati
         <Text className='availability-alert'>
           <span className='available-text'>Available </span>
           {'- Can be used on site. Please visit '}
-          <Link type='external' href={'https://www.nypl.org/locations/' + locationUrlEndpoint(location)}>{'New York Public Library - '}{locationShort}
+          <Link href={'https://www.nypl.org/locations/' + locationUrlEndpoint(location)}>{'New York Public Library - '}{locationShort}
           </Link> to submit a request in person
         </Text>)
     }
@@ -47,7 +46,7 @@ const RequestInfo = ({ isRecap, aeonUrl, available, divisionUrl, dueDate, locati
         <span className='unavailable-text'>Not available</span>
         {dueDate && dueDateAlert}
         <span>{' - Please '}
-          <Link type='external' href='https://www.nypl.org/about/divisions'>contact a librarian</Link>{' for assistance'}
+          <Link href='https://www.nypl.org/about/divisions'>contact a librarian</Link>{' for assistance'}
         </span>
       </div>)
   }
