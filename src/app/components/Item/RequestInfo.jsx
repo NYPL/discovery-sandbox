68c5890b64413d8a17cfd18d1c5df70642da19a2
Vera Kahn <verakahn@nypl.org>
2022-10-12 13:59:01 -0400

    add external link type and styles

--- src/app/components/Item/RequestInfo.jsx
+++ src/app/components/Item/RequestInfo.jsx
@@ -17,6 +17,7 @@ const RequestInfo = ({ isRecap, aeonUrl, available, divisionUrl, dueDate, locati
     if (isRecap) {
       //available recap item
       return <Link
+        type='external'
         className='info-link'
         href='https://www.nypl.org/help/request-research-materials'>
         How do I pick up this item and when will it be ready?
@@ -25,25 +26,30 @@ const RequestInfo = ({ isRecap, aeonUrl, available, divisionUrl, dueDate, locati
       return <Text className='availability-alert'>
         <span className='available-text'>Available by appointment</span>
         <span> at </span>
-        <Link className='division-link' href={divisionUrl}>{location}</Link>
+        <Link type='external' className='division-link' href={divisionUrl}>{location}</Link>
       </Text>
     }
     else {
       //available onsite item
       const locationShort = location.split('-')[0]
-      return <Text className='availability-alert'>
-        <span className='available-text'>Available </span>
-        - Can be used on site. Please visit <Link href={'https://www.nypl.org/locations/' + locationUrlEndpoint(location)}>New York Public Library - {locationShort}</Link> to submit a request in person</Text>
+      return (
+        <Text className='availability-alert'>
+          <span className='available-text'>Available </span>
+          {'- Can be used on site. Please visit '}
+          <Link type='external' href={'https://www.nypl.org/locations/' + locationUrlEndpoint(location)}>{'New York Public Library - '}{locationShort}
+          </Link> to submit a request in person
+        </Text>)
     }
   } else {
     const dueDateAlert = <span>{` - In use until ${dueDate}`}</span>
-    return <div className='availability-alert'>
-      <span className='unavailable-text'>Not available</span>
-      {dueDate && dueDateAlert}
-      <span> - Please
-        <Link href='https://www.nypl.org/about/divisions'> contact a librarian </Link>
-        for assistance</span>
-    </div>
+    return (
+      <div className='availability-alert'>
+        <span className='unavailable-text'>Not available</span>
+        {dueDate && dueDateAlert}
+        <span>{' - Please '}
+          <Link type='external' href='https://www.nypl.org/about/divisions'>contact a librarian</Link>{' for assistance'}
+        </span>
+      </div>)
   }
 }
 
