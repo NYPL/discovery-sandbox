43f29da6dc841c5af9cdbd20126f5e69f8f82160
Vera Kahn <verakahn@nypl.org>
2022-10-13 13:24:42 -0400

    finish tests for info links

--- test/unit/InformationLinks.test.js
+++ test/unit/InformationLinks.test.js
@@ -1,12 +1,13 @@
 import InformationLinks from "../../src/app/components/Item/InformationLinks"
+import { aeonUrl } from '../../src/app/components/Item/ItemTableRow'
 
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import libraryItems from '../fixtures/libraryItems';
 
-describe.only('information links', () => {
-  describe('recap items - available', () => {
+describe('information links', () => {
+  describe('recap items', () => {
     it('should display link to help section', () => {
       let item = libraryItems.requestable_ReCAP_available
       let component = shallow(<InformationLinks {...item} />)
@@ -23,33 +24,45 @@ describe.only('information links', () => {
     let component
     let item
     before(() => {
-      component = shallow(<InformationLinks {...item} />)
       item = libraryItems.requestable_nonReCAP_NYPL_not_available
+      component = shallow(<InformationLinks {...item} />)
     })
     it('should display unavailable', () => {
       expect(component.html()).to.include('<span class="unavailable-text">Not available</span>')
     })
     it('should display due date', () => {
-      console.log(item.dueDate)
       expect(component.html()).to.include('In use until 1996-07-21')
     })
   })
+
   describe('onsite items - available', () => {
     it('should display green available', () => {
-
+      const item = libraryItems.requestable_nonReCAP_NYPL
+      const component = shallow(<InformationLinks {...item} />)
+      expect(component.html()).to.include('<span class="available-text">Available </span>')
     })
-
     it('should link to LPA', () => {
+      const item = libraryItems.requestable_nonReCAP_NYPL_lpa
+      const component = shallow(<InformationLinks {...item} />)
+      expect(component.html()).to.include('href="https://www.nypl.org/locations/lpa"')
     })
     it('should link to Schwarzman', () => {
+      const item = libraryItems.requestable_nonReCAP_NYPL
+      const component = shallow(<InformationLinks {...item} />)
+      expect(component.html()).to.include('href="https://www.nypl.org/locations/schwarzman"')
     })
     it('should link to Schomburg', () => {
+      const item = libraryItems.requestable_nonReCAP_NYPL_schomburg
+      const component = shallow(<InformationLinks {...item} />)
+      expect(component.html()).to.include('href="https://www.nypl.org/locations/schomburg"')
     })
   })
 
   describe('special request items', () => {
     it('should link to the division where item is located', () => {
-
+      const item = libraryItems.aeonRequestableWithParams
+      const component = shallow(<InformationLinks {...item} computedAeonUrl={item.aeonUrl} />)
+      expect(component.html()).to.include(`href="${item.locationUrl}"`)
     })
   })
 })
