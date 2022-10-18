a74907e753a6a6d7fda603cc2a7f128f3746e107
Vera Kahn <verakahn@nypl.org>
2022-10-18 16:21:09 -0400

    fix a bunch of bibpage tests

--- test/unit/BibPage.test.js
+++ test/unit/BibPage.test.js
@@ -209,7 +209,7 @@ describe('BibPage', () => {
         context,
         childContextTypes: { router: PropTypes.object },
       });
-      itemTable = component.find('ItemTable');
+
     });
 
     it('has ItemsContainer', () => {
@@ -224,12 +224,13 @@ describe('BibPage', () => {
       expect(component.find('LibraryHoldings').length).to.equal(1);
     });
 
-    it('has item table with volume column', () => {
-      expect(itemTable.find('th').at(0).text()).to.equal('Vol/Date');
+    it('has an item table', () => {
+      itemTable = component.find('ItemTable');
+      expect(itemTable.length).to.be.at.least(1)
     });
 
     it('gets the format from holdings statement', () => {
-      expect(itemTable.find('td').at(1).text()).to.equal('PRINT');
+      expect(itemTable.find('td').at(0).text()).to.equal('PRINT');
     });
 
     it('displays any notes in the "Library Holdings" tab', () => {
