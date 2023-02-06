f137e53a2b0b86344f87d3e56a5aa50c03f30fd9
danamansana <danielappel@nypl.org>
2023-02-06 10:16:29 -0500

    Add correct extraction of itemPage search param

--- src/app/components/Item/ItemsContainer.jsx
+++ src/app/components/Item/ItemsContainer.jsx
@@ -22,7 +22,7 @@ class ItemsContainer extends React.Component {
     this.state = {
       showAll: false,
       js: false,
-      page: parseInt(this.props.itemPage.substring(10), 10) || 1,
+      page: parseInt(props.itemPage, 10) || 1,
       items: this.props.items || [],
     };
     this.query = context.router.location.query;
@@ -145,7 +145,7 @@ class ItemsContainer extends React.Component {
     const shortenItems = !this.props.shortenItems;
     let itemsToDisplay = [...this.state.items];
     let pagination = null;
-    
+
 
     if (
       this.state.js &&
