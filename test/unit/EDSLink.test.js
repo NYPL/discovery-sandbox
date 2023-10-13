b743bd1b42e77be55c8d33711695284c86c8c227
Diego Cohen <diego.cohen3@gmail.com>
2023-10-13 13:40:12 -0400

    Fix tests

--- test/unit/EDSLink.test.js
+++ test/unit/EDSLink.test.js
@@ -16,12 +16,13 @@ describe('EDSLink', () => {
 
   it('should have the right text', () => {
     const html = component.html();
-    expect(html).to.include('<span>New!</span> Try our <strong>Article Search</strong> to discover online journals, books, and more from home');
+    expect(html).to.include('<span>New!</span> Try our');
+    expect(html).to.include('to discover online journals, books, and more from home with your library card.')
   });
 
   it('should have a link with the correct text and href', () => {
     const edsLink = component.find('a')
-    expect(edsLink.text()).to.include('with your library card');
+    expect(edsLink.text()).to.include('Article Search');
     expect(edsLink.prop('href')).to.equal(
       'https://research.ebsco.com/c/2styhb'
     );
