dd0c03b817e4c9640e8728a2542d7cf57a2fa1a3
Diego Cohen <diego.cohen3@gmail.com>
2024-02-08 11:04:24 -0500

    Log appconig values

--- src/app/components/SubNav/SubNav.jsx
+++ src/app/components/SubNav/SubNav.jsx
@@ -29,7 +29,9 @@ SubNavLink.propTypes = {
 
 const SubNav = (props) => {
   const features = useSelector(state => state.features);
-  const { baseUrl, reverseProxyEnabled } = appConfig;
+  const { baseUrl, sierraUpgradeAugust2023, reverseProxyEnabled } = appConfig;
+  console.log("sierraUpgradeAugust2023", sierraUpgradeAugust2023)
+  console.log("reverseProxyEnabled", reverseProxyEnabled)
 
   return (
     <nav
